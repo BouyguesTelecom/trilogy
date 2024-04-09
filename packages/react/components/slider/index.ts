8c6c8515152ab4a9d654271833fec02c75ac8165
jumorata <jumorata@bouyguestelecom.fr>
2024-04-09 10:46:04 +0200

    Fix exports slider utils

--- packages/react/components/slider/index.ts
+++ packages/react/components/slider/index.ts
@@ -1,5 +1,6 @@
 import Slider from './Slider'
 import SliderItem from './item/SliderItem'
 import { ContentSize } from './SliderEnum'
+export default './utils/slider.js'
 
 export { Slider, SliderItem, ContentSize }
