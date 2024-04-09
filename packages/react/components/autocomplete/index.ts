58a07b8a9551b4fa49bae0b506f00303fcc9d44f
jumorata <jumorata@bouyguestelecom.fr>
2024-04-09 17:15:51 +0200

    Export autocomplete props

--- packages/react/components/autocomplete/index.ts
+++ packages/react/components/autocomplete/index.ts
@@ -2,4 +2,6 @@ import AutoComplete from './AutoComplete'
 import AutoCompleteItem from './item'
 import AutoCompleteMenu from './menu'
 
+export * from './AutoCompleteProps'
+
 export { AutoComplete, AutoCompleteItem, AutoCompleteMenu }
