0f3b753df163cf5c6edbcec8f8918fdbcd8d5017
jumorata <jumorata@bouyguestelecom.fr>
2024-04-09 16:56:11 +0200

    Update range onMouseUp => onChange event

--- examples/react-template/screens/Range.tsx
+++ examples/react-template/screens/Range.tsx
@@ -4,21 +4,22 @@ import { Section, Range } from '@trilogy-ds/react/components'
 export const RangeScreen = (): JSX.Element => {
   return (
     <Section>
-      <Range
-        min={0}
-        max={100}
-        labelValueCursorMin={'€'}
-        labelValueCursorMax={'€'}
-        valueCursorMin={0}
-        valueCursorMax={50}
-        label='This is the label'
-        idMin='min'
-        idMax='max'
-        nameMax='max'
-        nameMin='min'
-        onChangeMin={(e) => console.log(e)}
-        gap={0}
-      />
+        <Range
+            min={0}
+            max={100}
+            labelValueCursorMin={'€'}
+            labelValueCursorMax={'€'}
+            valueCursorMin={0}
+            valueCursorMax={50}
+            label='Ceci est un label'
+            idMin='min'
+            idMax='max'
+            nameMax='max'
+            nameMin='min'
+            onChangeMin={(e) => console.log(e)}
+            onChangeMax={(e) => console.log(e)}
+            gap={0}
+        />
     </Section>
   )
 }
