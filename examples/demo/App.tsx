93eb2fcafc758e0eeb67ae86901b1016f8806052
jumorata <jumorata@bouyguestelecom.fr>
2024-04-24 14:31:32 +0200

    Make working fonts

--- examples/demo/App.tsx
+++ examples/demo/App.tsx
@@ -1,9 +1,20 @@
-// import { View, Text } from 'react-native'
 import { Router } from '@trilogy-ds/react-template/router'
+import { useFonts } from 'expo-font';
+import { View } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'poppins-bold': require('./assets/fonts/ttf/poppins-bold.ttf'),
+    'poppins-medium': require('./assets/fonts/ttf/poppins-medium.ttf'),
+    'poppins-regular': require('./assets/fonts/ttf/poppins-regular.ttf'),
+    'poppins-semibold': require('./assets/fonts/ttf/poppins-semibold.ttf')
+  });
+
   return (
-    // <View><Text>test</Text></View>
-    <Router />
+    <View style={{ flex: 1 }}>
+      {fontsLoaded && (
+        <Router />
+      )}
+    </View>
   );
 }
