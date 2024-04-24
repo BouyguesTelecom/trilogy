29be00c28806aa824ab0f52136671621608e08f3
jumorata <jumorata@bouyguestelecom.fr>
2024-04-24 09:16:03 +0200

    🚀 Demo to expo

---
+++ examples/demo/App.tsx
@@ -0,0 +1,9 @@
+// import { View, Text } from 'react-native'
+import { Router } from '@trilogy-ds/react-template/router'
+
+export default function App() {
+  return (
+    // <View><Text>test</Text></View>
+    <Router />
+  );
+}
