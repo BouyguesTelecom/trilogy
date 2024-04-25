6a54e9e91ace03a3b7f3603b536f4f70c7e79103
Spicoud <sapicoud@bouyguestelecom.fr>
2024-04-25 14:40:21 +0200

    update slider

--- packages/react/components/slider/Slider.native.tsx
+++ packages/react/components/slider/Slider.native.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable indent */
-import React, { useRef, useState } from 'react'
-import { Dimensions, Platform, ScrollView, StyleSheet, Text, View } from 'react-native'
-import { SliderProps } from './SliderProps'
-import { getColorStyle, TrilogyColor } from '../../objects/facets/Color'
-import { Spacer, SpacerSize } from '../spacer'
-import { ComponentName } from '../enumsComponentsName'
+import React, { useRef, useState } from "react"
+import { Dimensions, Platform, ScrollView, StyleSheet, Text, View, } from "react-native"
+import { SliderProps } from "./SliderProps"
+import { getColorStyle, TrilogyColor } from "../../objects/facets/Color"
+import { Spacer, SpacerSize } from "../spacer"
+import { ComponentName } from "../enumsComponentsName"
 
-const { width } = Dimensions.get('screen')
+const { width } = Dimensions.get("screen")
 
 // const height = (width.width * 100) / 60 // 60%
 
@@ -23,41 +23,45 @@ const { width } = Dimensions.get('screen')
  * @param others
  */
 const Slider = ({
-                  children,
-                  doted,
-                  showNextSlide,
-                  contentSize,
-                  testId,
-                  accessibilityLabel,
-                  progressBar,
-                  bars,
-                  ...others
-                }: SliderProps): JSX.Element => {
+  children,
+  doted,
+  showNextSlide,
+  contentSize,
+  testId,
+  accessibilityLabel,
+  progressBar,
+  bars,
+  ...others
+}: SliderProps): JSX.Element => {
   const [activeItem, setActiveItem] = useState<number>(0)
   const [height, setHeight] = useState<number>(0)
 
-  const contentWidth = contentSize ? contentSize : showNextSlide ? width * 0.85 : width
+  const contentWidth = contentSize
+    ? contentSize
+    : showNextSlide
+    ? width * 0.85
+    : width
   const showNextSlideValue = showNextSlide ? 4 : 0
 
   const styles = StyleSheet.create({
     content: {
       width: contentWidth,
-      resizeMode: 'cover',
+      resizeMode: "cover",
       padding: showNextSlideValue,
-      height: '100%',
+      height: "100%",
     },
     dots: {
-      flexDirection: 'row',
-      position: 'absolute',
+      flexDirection: "row",
+      position: "absolute",
       bottom: doted ? -24 : -12,
-      alignSelf: 'center',
+      alignSelf: "center",
       fontSize: 30,
-      justifyContent: 'center',
-      alignItems: 'center',
+      justifyContent: "center",
+      alignItems: "center",
       marginLeft: 50,
     },
     textDots: {
-      color: '#C2C2C2',
+      color: getColorStyle(TrilogyColor.FONT, 1),
       fontSize: 30,
     },
     activeTextDot: {
@@ -65,14 +69,14 @@ const Slider = ({
       fontSize: 30,
     },
     progressBar: {
-      backgroundColor: '#C2C2C2',
+      backgroundColor: getColorStyle(TrilogyColor.FONT, 1),
       fontSize: 30,
       width: `${90 / React.Children.count(children)}%`,
       height: 6,
       borderRadius: 4,
       borderWidth: 1,
-      borderColor: '#C2C2C2',
-      overflow: 'hidden',
+      borderColor: getColorStyle(TrilogyColor.FONT, 1),
+      overflow: "hidden",
     },
     activeProgressBar: {
       backgroundColor: getColorStyle(TrilogyColor.MAIN),
@@ -82,14 +86,16 @@ const Slider = ({
       borderRadius: 4,
       borderWidth: 1,
       borderColor: getColorStyle(TrilogyColor.MAIN),
-      overflow: 'hidden',
+      overflow: "hidden",
     },
   })
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const changeHandler = ({ nativeEvent }: any) => {
     if (nativeEvent.layoutMeasurement.width === 0) return
-    const slide = Math.ceil(nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width)
+    const slide = Math.ceil(
+      nativeEvent.contentOffset.x / nativeEvent.layoutMeasurement.width
+    )
     if (slide !== activeItem) {
       setActiveItem(slide)
     }
@@ -102,14 +108,15 @@ const Slider = ({
     dot: boolean | undefined,
     progressBar: boolean | undefined,
     bar: boolean | undefined,
-    index: number,
+    index: number
   ) => {
     if (dot) {
       if (index === activeItem) return styles.activeTextDot
       return styles.textDots
     }
     if (bar || progressBar) {
-      if (index === activeItem) return { ...styles.activeProgressBar, marginRight: 8 }
+      if (index === activeItem)
+        return { ...styles.activeProgressBar, marginRight: 8 }
       return { ...styles.progressBar, marginRight: 8 }
     }
   }
@@ -123,7 +130,7 @@ const Slider = ({
       accessible={!!accessibilityLabel}
     >
       <ScrollView
-        style={{ position: 'absolute', alignSelf: 'center', width: width }}
+        style={{ position: "absolute", alignSelf: "center", width: width }}
         onScroll={changeHandler}
         snapToInterval={contentWidth}
         pagingEnabled
@@ -132,7 +139,10 @@ const Slider = ({
         showsHorizontalScrollIndicator={false}
         snapToAlignment='center'
         onLayout={(e) => {
-          scrollViewRef?.current?.scrollTo({ x: -SPACING_FOR_CARD_INSET, animated: false })
+          scrollViewRef?.current?.scrollTo({
+            x: -SPACING_FOR_CARD_INSET,
+            animated: false,
+          })
           setHeight(e.nativeEvent.layout.height)
         }}
         ref={scrollViewRef}
@@ -145,29 +155,36 @@ const Slider = ({
         }}
         contentContainerStyle={{
           // contentInset alternative for Android
-          paddingHorizontal: Platform.OS === 'android' ? SPACING_FOR_CARD_INSET : 0, // Horizontal spacing before and after the ScrollView
+          paddingHorizontal:
+            Platform.OS === "android" ? SPACING_FOR_CARD_INSET : 0, // Horizontal spacing before and after the ScrollView
         }}
         horizontal
       >
         {children
           ? // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          React.Children.map(children, (child: any) =>
-            React.cloneElement(child, {
-              style: [styles.content, child.props.style],
-            }),
-          )
+            React.Children.map(children, (child: any) =>
+              React.cloneElement(child, {
+                style: [styles.content, child.props.style],
+              })
+            )
           : children}
       </ScrollView>
       {(progressBar || doted || bars) && (
         <View style={styles.dots}>
           {children
-            ? React.Children.map(children, (_child: React.ReactNode, index: number) => (
-              <Text key={index} style={getStyles(doted, progressBar, bars, index)}>
-                {doted && '•'}
-              </Text>
-            ))
+            ? React.Children.map(
+                children,
+                (_child: React.ReactNode, index: number) => (
+                  <Text
+                    key={index}
+                    style={getStyles(doted, progressBar, bars, index)}
+                  >
+                    {doted && "•"}
+                  </Text>
+                )
+              )
             : children}
-          <Spacer size={SpacerSize.HUGE}/>
+          <Spacer size={SpacerSize.HUGE} />
         </View>
       )}
     </View>
