a3d029b78794642ac506dd1501a6a1869cb9c6ab
Spicoud <sapicoud@bouyguestelecom.fr>
2024-04-26 14:38:07 +0200

    remove slider and disclaimer components

--- packages/react/components/slider/index.ts
+++
@@ -1,6 +0,0 @@
-import Slider from './Slider'
-import SliderItem from './item/SliderItem'
-import { ContentSize } from './SliderEnum'
-export default './utils/slider.js'
-
-export { Slider, SliderItem, ContentSize }
