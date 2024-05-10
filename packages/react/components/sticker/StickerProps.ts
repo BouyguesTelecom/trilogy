669094b843a6d38ec7f05985e799360a5fa5aa8d
Spicoud <sapicoud@bouyguestelecom.fr>
2024-05-10 10:58:49 +0200

    remove deprecated props from sticker

--- packages/react/components/sticker/StickerProps.ts
+++ packages/react/components/sticker/StickerProps.ts
@@ -1,22 +1,11 @@
-import { Invertable } from "../../objects/facets/Invertable"
 import { Small } from "../../objects/facets/Small"
-import { AlertProps, VariantProps } from "../../objects/facets"
+import { VariantProps } from "../../objects/facets"
 import { Hat } from "../../objects/facets/Hat"
 import { StickerMarkup, StickerMarkupValues } from "./StickerEnum"
 
-export interface StickerProps
-  extends Small,
-    VariantProps,
-    AlertProps,
-    Hat,
-    Invertable {
+export interface StickerProps extends Small, VariantProps, Hat {
   children: React.ReactNode;
-  /**
-   * @deprecated
-   */
-  stretched?: boolean;
   className?: string;
   markup?: StickerMarkup | StickerMarkupValues;
   outlined?: boolean;
-  flag?: boolean;
 }
