1a203446ca5c520edae9a56655383d487abd25c9
Spicoud <sapicoud@bouyguestelecom.fr>
2024-05-28 14:15:47 +0200

    fix tabs style and add animation border

--- packages/react/components/tabs/item/TabsItem.native.tsx
+++ packages/react/components/tabs/item/TabsItem.native.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from "react"
-import { StyleSheet, TouchableOpacity, View } from "react-native"
-import { TabsItemProps } from "./TabsItemProps"
-import { getColorStyle, TrilogyColor } from "../../../objects/facets/Color"
-import { TypographyBold } from "../../../objects"
-import { ComponentName } from "../../enumsComponentsName"
-import { Icon } from "../../icon"
-import { Text, TextLevels } from "../../text"
+import React, {useEffect, useState} from "react"
+import {Animated, StyleSheet, TouchableOpacity, View} from "react-native"
+import {TabsItemProps} from "./TabsItemProps"
+import {getColorStyle, TrilogyColor} from "../../../objects/facets/Color"
+import {ComponentName} from "../../enumsComponentsName"
+import {Icon} from "../../icon"
+import {Text, TextLevels} from "../../text"
 
 /**
  * Tabs Item Component
@@ -15,18 +14,22 @@ import { Text, TextLevels } from "../../text"
  * @param iconName {IconNameValues | IconName} add icon name
  */
 const TabsItem = ({
-  active,
-  children,
-  onClick,
-  tabIndex,
-  iconName,
-  inverted,
-  disabled,
-  ...others
-}: TabsItemProps): JSX.Element => {
+                    active,
+                    children,
+                    onClick,
+                    tabIndex,
+                    iconName,
+                    inverted,
+                    disabled,
+                    ...others
+                  }: TabsItemProps): JSX.Element => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [activeItem, setActiveItem] = useState<boolean>(active || false)
   const [isPressIn, setInPressIn] = useState<boolean>(false)
+
+  const animatedBorderStyle = {
+    borderBottomWidth: active && 2,
+  }
 
   const getIconColor = React.useMemo(() => {
     if (inverted) {
@@ -42,7 +45,6 @@ const TabsItem = ({
     if (disabled) return "transparent"
     if (inverted) {
       if (active) return getColorStyle(TrilogyColor.BACKGROUND)
-      return getColorStyle(TrilogyColor.FONT, 1)
     }
     if (active) return getColorStyle(TrilogyColor.MAIN)
     return "transparent"
@@ -55,13 +57,18 @@ const TabsItem = ({
       paddingVertical: 6,
       paddingHorizontal: 14,
       position: "relative",
-      borderBottomWidth: 2,
       height: iconName ? 60 : 44,
-      borderBottomColor: getBorderColor,
     },
     text: {
       color: getColorStyle(getIconColor),
       textAlign: "center",
+    },
+    border: {
+      position: "absolute",
+      bottom: 0,
+      left: 0,
+      right: 0,
+      borderBottomColor: getBorderColor,
     },
   })
 
@@ -87,26 +94,19 @@ const TabsItem = ({
     >
       {iconName && (
         <View>
-          <Icon color={getIconColor} size='small' name={iconName} />
+          <Icon color={getIconColor} size='small' name={iconName}/>
         </View>
       )}
 
       {children &&
         (typeof children.valueOf() === "string" ? (
-          <Text
-            typo={
-              active
-                ? TypographyBold.TEXT_WEIGHT_SEMIBOLD
-                : TypographyBold.TEXT_WEIGHT_NORMAL
-            }
-            style={styles.text}
-            level={TextLevels.TWO}
-          >
+          <Text style={styles.text} level={TextLevels.TWO}>
             {String(children)}
           </Text>
         ) : (
           children
         ))}
+      <Animated.View style={[styles.border, animatedBorderStyle]}/>
     </TouchableOpacity>
   )
 }
