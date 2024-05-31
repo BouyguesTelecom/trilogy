c10e31ab5a53c10b4eb3b9a58a81f8ca5e1dd1c9
erwan <exavierdegetel@gmail.com>
2024-05-31 15:48:53 +0200

    add arrow navigation on options

--- packages/react/components/select/web/SelectDynamic.tsx
+++ packages/react/components/select/web/SelectDynamic.tsx
@@ -1,10 +1,10 @@
 import clsx from 'clsx'
-import React, { PropsWithChildren, useMemo } from 'react'
+import React, { PropsWithChildren, useCallback, useMemo, useState } from 'react'
 import ReactDOM from 'react-dom'
 
 import { hashClass } from '../../../helpers'
 import { Input } from '../../input'
-import { SelectProps } from '../SelectProps'
+import { SelectProps, SelectedValue } from '../SelectProps'
 import SelectOption from '../option'
 import { useTrilogyContext } from './../../../context'
 
@@ -26,23 +26,66 @@ const SelectDynamic = ({
 }: PropsWithChildren<SelectProps>): JSX.Element => {
   const { styled } = useTrilogyContext()
   const [focused, setIsFocused] = React.useState<boolean>(false)
-  const [selectedValues, setSelectedValues] = React.useState(selected)
+  const [selectedValues, setSelectedValues] = React.useState<SelectedValue>(selected)
   const [selectedName, setSelectedName] = React.useState<string[]>([])
   const reactId = React.useId()
   const selectClasses = React.useMemo(() => hashClass(styled, clsx('select', className)), [styled, className])
+  const [focusedIndex, setFocusedIndex] = useState<number>(-1)
 
   const onClickInput = React.useCallback(() => {
     setIsFocused((prev) => !prev)
   }, [])
 
+  const onKeyPressInput = React.useCallback((keyCode: number) => {
+    keyCode === 0 && setIsFocused((prev) => !prev)
+  }, [])
+
+  const isChecked = useCallback(
+    (value: string) => (multiple ? (selectedValues as (number | string)[])?.includes(value) : selectedValues === value),
+    [multiple, selectedValues],
+  )
+
+  const setNewSelectedValues = useCallback(
+    ({ isChecked, children, label, value }: { isChecked: boolean; children: string; label: string; value: any }) => {
+      if (isChecked) {
+        setSelectedValues((prev) => {
+          switch (true) {
+            case Array.isArray(prev) && nullable:
+              setSelectedName((prev) => prev.filter((txt) => ![children, label].includes(txt)))
+              return prev.filter((item: string | number) => item !== value)
+            case Array.isArray(prev) && !nullable:
+              return prev
+            case !Array.isArray(prev) && !nullable:
+              setIsFocused(false)
+              return prev
+            case !Array.isArray(prev) && nullable:
+              setIsFocused(false)
+              setSelectedName([])
+              return undefined
+            default:
+              return value
+          }
+        })
+      }
+      if (!isChecked) {
+        setSelectedName((prev) => [...prev, children || label])
+        setSelectedValues((prev) => {
+          if (Array.isArray(prev)) return [...prev, value]
+          setIsFocused(false)
+          setSelectedName([children || label])
+          return value
+        })
+      }
+    },
+    [],
+  )
+
   React.useEffect(() => {
     setSelectedValues(selected)
-
     const setInputValue = () => {
       const labelSelected = React.Children.map(children, (child) => {
         if (!React.isValidElement(child)) return false
         const label = child.props.children || child.props.label
-
         switch (true) {
           case (Array.isArray(selected) && (selected as (number | string)[]).includes(child.props.value)) ||
             (!Array.isArray(selected) && child.props.value === selected):
@@ -55,6 +98,37 @@ const SelectDynamic = ({
     }
     setInputValue()
   }, [selected])
+
+  React.useEffect(() => {
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (['ArrowDown', 'ArrowUp', 'Enter'].includes(e.key)) e.preventDefault()
+      switch (true) {
+        case e.key === 'ArrowDown':
+          options && setFocusedIndex((prev) => (prev + 1) % options.length)
+          break
+        case e.key === 'ArrowUp' && focusedIndex !== -1:
+          options && setFocusedIndex((prev) => (prev - 1 + options.length) % options.length)
+          break
+        case e.key === 'ArrowUp' && focusedIndex === -1:
+          options && setFocusedIndex(() => options.length - 1)
+          break
+        case e.key === 'Enter' && focusedIndex !== -1:
+          const childs = (children as React.ReactElement[])[focusedIndex]
+          setNewSelectedValues({
+            children: childs.props.children,
+            label: childs.props.label,
+            value: childs.props.value,
+            isChecked: isChecked(childs.props.value),
+          })
+          break
+      }
+    }
+    focused && document.addEventListener('keydown', onKeyDown)
+    !focused && setFocusedIndex(-1)
+    return () => {
+      document.removeEventListener('keydown', onKeyDown)
+    }
+  }, [focused, focusedIndex])
 
   const modal = useMemo(() => {
     return <div role='presentation' className='select-trilogy_modal_open' onClick={() => setIsFocused(false)}></div>
@@ -74,56 +148,17 @@ const SelectDynamic = ({
         }
       }
 
-      const isChecked = multiple
-        ? (selectedValues as (number | string)[])?.includes(child.props.value)
-        : selectedValues === child.props.value
-
-      const setNewSelectedValues = () => {
-        if (isChecked) {
-          setSelectedValues((prev) => {
-            switch (true) {
-              case Array.isArray(prev) && nullable:
-                setSelectedName((prev) =>
-                  prev.filter((txt) => ![child.props.children, child.props.label].includes(txt)),
-                )
-                return (prev as (number | string)[]).filter((item: string | number) => item !== child.props.value)
-
-              case Array.isArray(prev) && !nullable:
-                return prev
-
-              case !Array.isArray(prev) && !nullable:
-                setIsFocused(false)
-                return prev
-
-              case !Array.isArray(prev) && nullable:
-                setIsFocused(false)
-                setSelectedName([])
-                return undefined
-
-              default:
-                return child.props.value
-            }
-          })
-        }
-
-        if (!isChecked) {
-          setSelectedValues((prev) => {
-            if (Array.isArray(prev)) {
-              setSelectedName((prev) => [...prev, child.props.children || child.props.label])
-              return [...prev, child.props.value]
-            }
-            setIsFocused(false)
-            setSelectedName([child.props.children || child.props.label])
-            return child.props.value
-          })
-        }
-      }
-
       const props = {
         ...child.props,
-        checked: isChecked,
+        checked: isChecked(child.props.value),
+        focused: focusedIndex === index,
         onClick: () => {
-          setNewSelectedValues()
+          setNewSelectedValues({
+            children: child.props.children,
+            label: child.props.label,
+            value: child.props.value,
+            isChecked: isChecked(child.props.value),
+          })
           onChange &&
             onChange({
               selectValue: clickEventValue(child.props.value),
@@ -135,7 +170,7 @@ const SelectDynamic = ({
       }
       return <SelectOption {...props} key={`${reactId}_${index}`} />
     })
-  }, [multiple, nullable, selectedValues])
+  }, [multiple, nullable, selectedValues, focusedIndex])
 
   return (
     <div className={selectClasses}>
@@ -151,6 +186,7 @@ const SelectDynamic = ({
         onBlur={onBlur}
         onClick={onClickInput}
         className={hashClass(styled, clsx(focused && 'focus'))}
+        onKeyPress={(e) => onKeyPressInput(e.inputKeyCode)}
         {...{ readOnly: true, id }}
       />
       {focused && <div className={hashClass(styled, clsx('select-options'))}>{options}</div>}
@@ -158,5 +194,4 @@ const SelectDynamic = ({
     </div>
   )
 }
-
 export default SelectDynamic
