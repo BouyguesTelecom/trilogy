32052464be3afaeb0242f219d44cd4a3c5f7bbab
erwan <exavierdegetel@gmail.com>
2024-06-06 14:24:37 +0200

    add unique select according platform os

--- packages/react/components/select/Select.native.tsx
+++ packages/react/components/select/Select.native.tsx
@@ -1,7 +1,4 @@
-import { Picker } from '@react-native-picker/picker'
 import React, { useCallback, useEffect, useState } from 'react'
-import { Platform, StyleSheet, View } from 'react-native'
-import { TrilogyColor, getColorStyle } from '../../objects'
 import { ComponentName } from '../enumsComponentsName'
 import { IconName } from '../icon'
 import { Input } from '../input'
@@ -28,6 +25,7 @@ const Select = ({
   onChange,
   placeholder,
   disabled,
+  multiple,
   ...others
 }: SelectProps): JSX.Element => {
   const [selectedValue, setSelectedValue] = useState<string | number>(selected as string)
@@ -37,59 +35,22 @@ const Select = ({
     setSelectedValue((selected as string) || '')
   }, [selected])
 
-  const styles = StyleSheet.create({
-    select: {
-      width: '100%',
-      backgroundColor: disabled ? getColorStyle(TrilogyColor.DISABLED, 1) : TrilogyColor.BACKGROUND,
-      borderColor: disabled ? getColorStyle(TrilogyColor.DISABLED, 1) : getColorStyle(TrilogyColor.FONT, 1),
-      borderWidth: 1,
-      borderRadius: 4,
-      zIndex: 3, // works on ios
-      overflow: 'hidden',
-      flexDirection: 'row',
-      alignItems: 'center',
-      height: 50,
-    },
-  })
-
   const handleOpenCloseModal = useCallback(() => {
     setDisplay((prev) => !prev)
   }, [])
 
-  if (Platform.OS === 'ios') {
-    return (
-      <>
-        <Input
-          placeholder={label}
-          hasIcon={!!iconName}
-          customIconLeft={iconName}
-          {...{ editable: false, onPressIn: handleOpenCloseModal }}
-        />
-        <Modal active={display} onClose={handleOpenCloseModal} closeIcon swipable={false}>
-          {children}
-        </Modal>
-      </>
-    )
-  }
-
   return (
-    <View style={styles.select}>
-      <Picker
-        placeholder={placeholder}
-        selectedValue={selectedValue}
-        style={{ width: '100%', height: '100%' }}
-        onValueChange={(itemValue: number | string) => {
-          if (onChange) {
-            onChange(itemValue)
-          }
-          setSelectedValue(itemValue)
-          setDisplay(false)
-        }}
-        mode={'dialog'}
-      >
+    <>
+      <Input
+        placeholder={label}
+        hasIcon={!!iconName}
+        customIconLeft={iconName}
+        {...{ editable: false, onPressIn: handleOpenCloseModal }}
+      />
+      <Modal active={display} onClose={handleOpenCloseModal} closeIcon swipable={false}>
         {children}
-      </Picker>
-    </View>
+      </Modal>
+    </>
   )
 }
 
