f0cd9b27ea2608efb80fcaa875d1209fe2a4cfc2
JulienMora <julien.moratalla@gmail.com>
2024-06-18 14:08:44 +0200

    Update default theme font

--- packages/react/context/providerTheme.native.tsx
+++ packages/react/context/providerTheme.native.tsx
@@ -17,7 +17,7 @@ export const defaultIcons = {}
 export const defaultTheme: ITrilogyTheme = {
   icons: defaultIcons,
   colors: colors,
-  fontFamily: {}
+  fontFamily: { 'regular': 'poppins-regular', 'medium': 'poppins-medium', 'bold': 'poppins-semibold' }
 }
 
 export const defaultContextValue = {
