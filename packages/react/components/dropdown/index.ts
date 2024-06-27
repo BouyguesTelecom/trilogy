00de92306f0e5e7b9506235615fb13e8b5dc669e
erwan <exavierdegetel@gmail.com>
2024-06-27 14:48:37 +0200

    delete dropdown component

--- packages/react/components/dropdown/index.ts
+++
@@ -1,7 +0,0 @@
-import Dropdown from './Dropdown'
-import DropdownTrigger from './trigger'
-import DropdownMenu from './menu'
-import DropdownItem from './item'
-import DropdownDivider from './divider'
-
-export { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, DropdownDivider }
