83d368900d0060cd7d8775ce96f8bdebfc99b1ac
erwan <exavierdegetel@gmail.com>
2024-06-28 16:07:10 +0200

    fix tests

--- packages/react/components/icon/text/TextIcon.tsx
+++ packages/react/components/icon/text/TextIcon.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import {Icon} from "@/components/icon"
+import Icon from "@/components/icon/Icon"
 import { IconProps } from "@/components/icon/IconProps"
 import {
   IconPosition,
