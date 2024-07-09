a4357d788f544de0e0b4f01cdedfd0f8bcd6f3ba
Weckr Stephane <swecker@bouyguestelecom.fr>
2024-07-09 11:05:11 +0200

    Adding test

--- packages/react/components/otp/test/Otp.test.tsx
+++ packages/react/components/otp/test/Otp.test.tsx
@@ -8,7 +8,6 @@ describe("Otp", () => {
     codeSize: 6,
     disabled: false,
     error: false,
-    activated: false,
     onCompleted: jest.fn(),
     onChange: jest.fn(),
     onFocus: jest.fn(),
@@ -27,12 +26,29 @@ describe("Otp", () => {
     expect(defaultProps.onFocus).toHaveBeenCalledWith(true);
   });
 
-  it("calls onChange when we paste value", () => {
+  it("calls onChange when a num key is pressed", () => {
     const { getByTestId } = render(<Otp {...defaultProps} />);
     const container = getByTestId("otp-input");
-    fireEvent.click(container);
-    fireEvent.paste(container, { clipboardData: { getData: () => "123456" } });
-    expect(defaultProps.onChange).toHaveBeenCalledWith("123456");
+    fireEvent.input(container.children[0], { target: { value: "1"} });
+    expect(defaultProps.onChange).toHaveBeenCalledWith("1_____");
+  });
+
+  it("calls onChange when value is pasted (value.length > 1)", () => {
+    const { getByTestId } = render(<Otp {...defaultProps} />);
+    const input = getByTestId("otp-input");
+    fireEvent.paste(input, {
+      clipboardData: { getData: () => ('1234') }
+    });
+    fireEvent.input(input.children[0], { target: { value: '1234' } });
+    expect(defaultProps.onChange).toHaveBeenCalledWith('1234__');
+  });
+
+
+  it("calls onCompleted with '123456' when value 123456 is pasted", () => {
+    const { getByTestId } = render(<Otp {...defaultProps} />);
+    const input = getByTestId("otp-input");
+    fireEvent.input(input.children[0], { target: { value: '123456' } });
+    expect(defaultProps.onCompleted).toHaveBeenCalledWith('123456');
   });
 
 });
