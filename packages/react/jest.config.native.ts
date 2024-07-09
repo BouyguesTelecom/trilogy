c4f08be414f85591c0dd5329870250e92127336e
erwan <exavierdegetel@gmail.com>
2024-07-09 11:33:55 +0200

    add alert autolayout and badge test

--- packages/react/jest.config.native.ts
+++ packages/react/jest.config.native.ts
@@ -9,6 +9,6 @@ export default {
   testRegex: ['/components/.*/test/.*native'],
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  transformIgnorePatterns: ['node_modules/(?!(@react-native|react-native)/)'],
+  transformIgnorePatterns: ['node_modules/(?!(@react-native|react-native|react-native-toast-message)/)'],
   setupFilesAfterEnv: ['<rootDir>/jest-setup.native.ts'],
 }
