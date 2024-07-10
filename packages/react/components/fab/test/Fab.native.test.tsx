8b5467e669d22a0154314ba2e2d0e0382facd689
erwan <exavierdegetel@gmail.com>
2024-07-10 15:17:58 +0200

    add countdown divider fab tests

---
+++ packages/react/components/fab/test/Fab.native.test.tsx
@@ -0,0 +1,41 @@
+import { render, screen, userEvent } from '@testing-library/react-native'
+import { IconName } from '@trilogy-ds/assets'
+import * as React from 'react'
+import Fab from '../Fab.native'
+
+jest.useFakeTimers()
+
+describe('Fab component', () => {
+  it('renders the children correctly', () => {
+    render(
+      <Fab testId='fab' iconName={IconName.INFOS_CIRCLE} right={20} bottom={15}>
+        New Conversation
+      </Fab>,
+    )
+    expect(screen.getByTestId('fab')).toBeOnTheScreen()
+  })
+
+  it('shoud be clickable click', async () => {
+    const onClick = jest.fn()
+
+    render(
+      <Fab testId='fab' onClick={onClick} iconName={IconName.INFOS_CIRCLE} right={20} bottom={15}>
+        New Conversation
+      </Fab>,
+    )
+    const user = userEvent.setup()
+    await user.press(screen.getByTestId('fab'))
+    expect(onClick).toHaveBeenCalled()
+  })
+
+  it('shoud be extended', async () => {
+    const onClick = jest.fn()
+
+    render(
+      <Fab extended testId='fab' onClick={onClick} iconName={IconName.INFOS_CIRCLE} right={20} bottom={15}>
+        New Conversation
+      </Fab>,
+    )
+    expect(screen.getByText('New Conversation')).toBeOnTheScreen()
+  })
+})
