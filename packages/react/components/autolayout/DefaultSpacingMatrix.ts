fbcd777783fda4b8cb9755ac3139fe73a254f177
JulienMora <julien.moratalla@gmail.com>
2024-07-12 09:41:48 +0200

    Update spacer size rules

--- packages/react/components/autolayout/DefaultSpacingMatrix.ts
+++ packages/react/components/autolayout/DefaultSpacingMatrix.ts
@@ -1,7 +1,7 @@
 import { SpacerSize } from '@/components/spacer'
 import { SpacingMatrix, SpacingMatrixMode } from './SpacingMatrix'
 
-const { HUGE, LARGE, MEDIUM, SMALL, NONE } = SpacerSize
+const { EIGHT, SEVEN, SIX, FIVE, FOUR, THREE, TWO, ONE } = SpacerSize
 
 export const { INSERT_SPACE_BETWEEN } = SpacingMatrixMode
 
@@ -9,108 +9,108 @@ export const { INSERT_SPACE_BETWEEN } = SpacingMatrixMode
  * Spacing matrix for defining spacings between component types
  */
 export const DEFAULT_SPACING_MATRIX: SpacingMatrix = [
-  [INSERT_SPACE_BETWEEN, 'Box', 'Box', SMALL],
-  [INSERT_SPACE_BETWEEN, 'Box', 'Button', LARGE],
-  [INSERT_SPACE_BETWEEN, 'Box', 'Title', MEDIUM],
+  [INSERT_SPACE_BETWEEN, 'Box', 'Box', TWO],
+  [INSERT_SPACE_BETWEEN, 'Box', 'Button', SEVEN],
+  [INSERT_SPACE_BETWEEN, 'Box', 'Title', THREE],
 
-  [INSERT_SPACE_BETWEEN, 'Button', 'Button', MEDIUM],
-  [INSERT_SPACE_BETWEEN, 'Title', 'Button', MEDIUM],
-  [INSERT_SPACE_BETWEEN, 'Button', 'Divider', SMALL],
-  [INSERT_SPACE_BETWEEN, 'Button', 'Link', LARGE],
-  [INSERT_SPACE_BETWEEN, 'Button', 'Title', HUGE],
-  [INSERT_SPACE_BETWEEN, 'Text', 'Button', MEDIUM],
-  [INSERT_SPACE_BETWEEN, 'Text', 'Title', SMALL],
+  [INSERT_SPACE_BETWEEN, 'Button', 'Button', THREE],
+  [INSERT_SPACE_BETWEEN, 'Title', 'Button', THREE],
+  [INSERT_SPACE_BETWEEN, 'Button', 'Divider', TWO],
+  [INSERT_SPACE_BETWEEN, 'Button', 'Link', SEVEN],
+  [INSERT_SPACE_BETWEEN, 'Button', 'Title', EIGHT],
+  [INSERT_SPACE_BETWEEN, 'Text', 'Button', THREE],
+  [INSERT_SPACE_BETWEEN, 'Text', 'Title', TWO],
 
-  [INSERT_SPACE_BETWEEN, 'default', 'Divider', LARGE],
-  [INSERT_SPACE_BETWEEN, 'Divider', 'Button', SMALL],
-  [INSERT_SPACE_BETWEEN, 'Divider', 'default', LARGE],
+  [INSERT_SPACE_BETWEEN, 'default', 'Divider', SEVEN],
+  [INSERT_SPACE_BETWEEN, 'Divider', 'Button', TWO],
+  [INSERT_SPACE_BETWEEN, 'Divider', 'default', SEVEN],
 
-  [INSERT_SPACE_BETWEEN, 'Image', 'Title', LARGE],
+  [INSERT_SPACE_BETWEEN, 'Image', 'Title', SEVEN],
 
-  [INSERT_SPACE_BETWEEN, 'Input', 'Input', LARGE],
-  [INSERT_SPACE_BETWEEN, 'Input', 'Progress', MEDIUM],
+  [INSERT_SPACE_BETWEEN, 'Input', 'Input', SEVEN],
+  [INSERT_SPACE_BETWEEN, 'Input', 'Progress', THREE],
 
-  [INSERT_SPACE_BETWEEN, 'Link', 'Button', LARGE],
-  [INSERT_SPACE_BETWEEN, 'Link', 'Link', LARGE],
+  [INSERT_SPACE_BETWEEN, 'Link', 'Button', SEVEN],
+  [INSERT_SPACE_BETWEEN, 'Link', 'Link', SEVEN],
 
-  [INSERT_SPACE_BETWEEN, 'Progress', 'Progress', SMALL],
+  [INSERT_SPACE_BETWEEN, 'Progress', 'Progress', TWO],
 
-  [INSERT_SPACE_BETWEEN, 'ProgressRadial', 'ProgressRadial', SMALL],
+  [INSERT_SPACE_BETWEEN, 'ProgressRadial', 'ProgressRadial', TWO],
 
-  [INSERT_SPACE_BETWEEN, 'Radio', 'Button', MEDIUM],
+  [INSERT_SPACE_BETWEEN, 'Radio', 'Button', THREE],
 
-  [INSERT_SPACE_BETWEEN, 'Sticker', 'Sticker', SMALL],
+  [INSERT_SPACE_BETWEEN, 'Sticker', 'Sticker', TWO],
 
-  [INSERT_SPACE_BETWEEN, 'Text', 'Radio', HUGE],
-  [INSERT_SPACE_BETWEEN, 'Text', 'Box', SMALL],
-  [INSERT_SPACE_BETWEEN, 'Text', 'Title', LARGE],
+  [INSERT_SPACE_BETWEEN, 'Text', 'Radio', EIGHT],
+  [INSERT_SPACE_BETWEEN, 'Text', 'Box', TWO],
+  [INSERT_SPACE_BETWEEN, 'Text', 'Title', SEVEN],
 
-  [INSERT_SPACE_BETWEEN, 'Title', 'Box', SMALL],
-  [INSERT_SPACE_BETWEEN, 'Title', 'Text', SMALL],
-  [INSERT_SPACE_BETWEEN, 'Title', 'Card', MEDIUM],
-  [INSERT_SPACE_BETWEEN, 'Title', 'Icon', MEDIUM],
-  [INSERT_SPACE_BETWEEN, 'Title', 'Title', SMALL],
-  [INSERT_SPACE_BETWEEN, 'Title', 'Tag', LARGE],
-  [INSERT_SPACE_BETWEEN, 'Title', 'default', MEDIUM],
-  [INSERT_SPACE_BETWEEN, 'Title', 'Text', SMALL],
+  [INSERT_SPACE_BETWEEN, 'Title', 'Box', TWO],
+  [INSERT_SPACE_BETWEEN, 'Title', 'Text', TWO],
+  [INSERT_SPACE_BETWEEN, 'Title', 'Card', THREE],
+  [INSERT_SPACE_BETWEEN, 'Title', 'Icon', THREE],
+  [INSERT_SPACE_BETWEEN, 'Title', 'Title', TWO],
+  [INSERT_SPACE_BETWEEN, 'Title', 'Tag', SEVEN],
+  [INSERT_SPACE_BETWEEN, 'Title', 'default', THREE],
+  [INSERT_SPACE_BETWEEN, 'Title', 'Text', TWO],
 
-  [INSERT_SPACE_BETWEEN, 'Tag', 'Text', MEDIUM],
-  [INSERT_SPACE_BETWEEN, 'Tag', 'Title', LARGE],
+  [INSERT_SPACE_BETWEEN, 'Tag', 'Text', THREE],
+  [INSERT_SPACE_BETWEEN, 'Tag', 'Title', SEVEN],
 
-  [INSERT_SPACE_BETWEEN, 'AutoLayout', 'default', NONE],
-  [INSERT_SPACE_BETWEEN, 'default', 'AutoLayout', NONE],
+  [INSERT_SPACE_BETWEEN, 'AutoLayout', 'default', ONE],
+  [INSERT_SPACE_BETWEEN, 'default', 'AutoLayout', ONE],
 
-  [INSERT_SPACE_BETWEEN, 'Container', 'default', NONE],
-  [INSERT_SPACE_BETWEEN, 'default', 'Container', NONE],
+  [INSERT_SPACE_BETWEEN, 'Container', 'default', ONE],
+  [INSERT_SPACE_BETWEEN, 'default', 'Container', ONE],
 
-  [INSERT_SPACE_BETWEEN, 'Spacer', 'default', NONE],
-  [INSERT_SPACE_BETWEEN, 'default', 'Spacer', NONE],
+  [INSERT_SPACE_BETWEEN, 'Spacer', 'default', ONE],
+  [INSERT_SPACE_BETWEEN, 'default', 'Spacer', ONE],
 
-  [INSERT_SPACE_BETWEEN, 'View', 'default', NONE],
-  [INSERT_SPACE_BETWEEN, 'default', 'View', NONE],
+  [INSERT_SPACE_BETWEEN, 'View', 'default', ONE],
+  [INSERT_SPACE_BETWEEN, 'default', 'View', ONE],
 
-  [INSERT_SPACE_BETWEEN, 'Radio', 'default', HUGE],
-  [INSERT_SPACE_BETWEEN, 'default', 'Radio', HUGE],
+  [INSERT_SPACE_BETWEEN, 'Radio', 'default', EIGHT],
+  [INSERT_SPACE_BETWEEN, 'default', 'Radio', EIGHT],
 
-  [INSERT_SPACE_BETWEEN, 'Checkbox', 'default', HUGE],
-  [INSERT_SPACE_BETWEEN, 'default', 'Checkbox', HUGE],
+  [INSERT_SPACE_BETWEEN, 'Checkbox', 'default', EIGHT],
+  [INSERT_SPACE_BETWEEN, 'default', 'Checkbox', EIGHT],
 
-  [INSERT_SPACE_BETWEEN, 'default', 'Otp', HUGE],
+  [INSERT_SPACE_BETWEEN, 'default', 'Otp', EIGHT],
 
-  [INSERT_SPACE_BETWEEN, 'Checkbox', 'default', HUGE],
-  [INSERT_SPACE_BETWEEN, 'default', 'Checkbox', HUGE],
+  [INSERT_SPACE_BETWEEN, 'Checkbox', 'default', EIGHT],
+  [INSERT_SPACE_BETWEEN, 'default', 'Checkbox', EIGHT],
 
-  [INSERT_SPACE_BETWEEN, 'Columns', 'Columns', SMALL],
-  [INSERT_SPACE_BETWEEN, 'Columns', 'default', MEDIUM],
-  [INSERT_SPACE_BETWEEN, 'default', 'Columns', MEDIUM],
+  [INSERT_SPACE_BETWEEN, 'Columns', 'Columns', TWO],
+  [INSERT_SPACE_BETWEEN, 'Columns', 'default', THREE],
+  [INSERT_SPACE_BETWEEN, 'default', 'Columns', THREE],
 
-  [INSERT_SPACE_BETWEEN, 'Link', 'default', MEDIUM],
-  [INSERT_SPACE_BETWEEN, 'default', 'Link', MEDIUM],
+  [INSERT_SPACE_BETWEEN, 'Link', 'default', THREE],
+  [INSERT_SPACE_BETWEEN, 'default', 'Link', THREE],
 
-  [INSERT_SPACE_BETWEEN, 'Input', 'default', MEDIUM],
-  [INSERT_SPACE_BETWEEN, 'default', 'Input', MEDIUM],
+  [INSERT_SPACE_BETWEEN, 'Input', 'default', THREE],
+  [INSERT_SPACE_BETWEEN, 'default', 'Input', THREE],
 
-  [INSERT_SPACE_BETWEEN, 'Text', 'default', MEDIUM],
-  [INSERT_SPACE_BETWEEN, 'default', 'Text', MEDIUM],
+  [INSERT_SPACE_BETWEEN, 'Text', 'default', THREE],
+  [INSERT_SPACE_BETWEEN, 'default', 'Text', THREE],
 
-  [INSERT_SPACE_BETWEEN, 'Icon', 'default', MEDIUM],
+  [INSERT_SPACE_BETWEEN, 'Icon', 'default', THREE],
 
-  [INSERT_SPACE_BETWEEN, 'Button', 'default', MEDIUM],
-  [INSERT_SPACE_BETWEEN, 'default', 'Button', MEDIUM],
+  [INSERT_SPACE_BETWEEN, 'Button', 'default', THREE],
+  [INSERT_SPACE_BETWEEN, 'default', 'Button', THREE],
 
-  [INSERT_SPACE_BETWEEN, 'default', 'default', SMALL],
+  [INSERT_SPACE_BETWEEN, 'default', 'default', TWO],
 
-  [INSERT_SPACE_BETWEEN, 'Image', 'Text', MEDIUM],
-  [INSERT_SPACE_BETWEEN, 'Image', 'Title', MEDIUM],
+  [INSERT_SPACE_BETWEEN, 'Image', 'Text', THREE],
+  [INSERT_SPACE_BETWEEN, 'Image', 'Title', THREE],
 
-  [INSERT_SPACE_BETWEEN, 'View', 'Card', MEDIUM],
-  [INSERT_SPACE_BETWEEN, 'Card', 'View', MEDIUM],
+  [INSERT_SPACE_BETWEEN, 'View', 'Card', THREE],
+  [INSERT_SPACE_BETWEEN, 'Card', 'View', THREE],
 
-  [INSERT_SPACE_BETWEEN, 'Radio', 'Divider', SMALL],
-  [INSERT_SPACE_BETWEEN, 'Divider', 'Radio', SMALL],
-  [INSERT_SPACE_BETWEEN, 'Radio', 'Radio', MEDIUM],
+  [INSERT_SPACE_BETWEEN, 'Radio', 'Divider', TWO],
+  [INSERT_SPACE_BETWEEN, 'Divider', 'Radio', TWO],
+  [INSERT_SPACE_BETWEEN, 'Radio', 'Radio', THREE],
 
-  [INSERT_SPACE_BETWEEN, 'Checkbox', 'Divider', SMALL],
-  [INSERT_SPACE_BETWEEN, 'Divider', 'Checkbox', SMALL],
-  [INSERT_SPACE_BETWEEN, 'Checkbox', 'Checkbox', SMALL],
+  [INSERT_SPACE_BETWEEN, 'Checkbox', 'Divider', TWO],
+  [INSERT_SPACE_BETWEEN, 'Divider', 'Checkbox', TWO],
+  [INSERT_SPACE_BETWEEN, 'Checkbox', 'Checkbox', TWO],
 ]
