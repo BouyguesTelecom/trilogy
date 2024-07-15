45cbcfecc61c9884e851ed136326df9102e66720
JulienMora <julien.moratalla@gmail.com>
2024-07-15 09:29:48 +0200

    Update matrice

--- packages/styles/scripts/autolayout.ts
+++ packages/styles/scripts/autolayout.ts
@@ -3,66 +3,79 @@ import { INSERT_SPACE_BETWEEN } from "@trilogy-ds/react/components/autolayout/De
 import { SpacerSize } from "@trilogy-ds/react/components/spacer";
 import { SpacingMatrixMode } from "@trilogy-ds/react/components/autolayout/SpacingMatrix";
 
-type DefaultSpacingMatrix = Array<Array<SpacingMatrixMode | string | SpacerSize>>;
+type DefaultSpacingMatrix = Array<[SpacingMatrixMode, string, (string | SpacerSize)?, SpacerSize?, SpacerSize?]>;
 
-const { NINE, EIGHT, SEVEN, SIX, FIVE, FOUR, THREE, TWO, ONE } = SpacerSize;
+const { THREE, FOUR, FIVE, EIGHT } = SpacerSize;
 
 const DEFAULT_SPACING_MATRIX: DefaultSpacingMatrix = [
-  [INSERT_SPACE_BETWEEN, 'Box', FIVE],
+  [INSERT_SPACE_BETWEEN, 'Box', '', THREE, FIVE],
   [INSERT_SPACE_BETWEEN, 'Box', 'default', FIVE],
   [INSERT_SPACE_BETWEEN, 'default', 'Box', THREE],
   [INSERT_SPACE_BETWEEN, 'Title', 'Button', THREE],
-  [INSERT_SPACE_BETWEEN, 'Text', THREE],
-  [INSERT_SPACE_BETWEEN, 'Card', FIVE],
-  [INSERT_SPACE_BETWEEN, 'Button', FIVE],
-  [INSERT_SPACE_BETWEEN, 'Accordions', FIVE],
-  [INSERT_SPACE_BETWEEN, 'Alert', FIVE],
-  [INSERT_SPACE_BETWEEN, 'Field', THREE],
-  [INSERT_SPACE_BETWEEN, 'Chips-list', FIVE],
-  [INSERT_SPACE_BETWEEN, 'Tags', FIVE],
-  [INSERT_SPACE_BETWEEN, 'Chips-list', FIVE],
+  [INSERT_SPACE_BETWEEN, 'Text', '', THREE, FOUR],
+  [INSERT_SPACE_BETWEEN, 'Card', '', THREE, FIVE],
+  [INSERT_SPACE_BETWEEN, 'Button', '', THREE],
+  [INSERT_SPACE_BETWEEN, 'Accordions', '', THREE],
+  [INSERT_SPACE_BETWEEN, 'Alert', '', THREE],
+  [INSERT_SPACE_BETWEEN, 'Chips-list', '', THREE, FIVE],
+  [INSERT_SPACE_BETWEEN, 'Tags', '', THREE],
+  [INSERT_SPACE_BETWEEN, 'Chips-list', '', THREE],
 ];
 
 const createBodyAutolayoutSCSS = (spacingMatrix: DefaultSpacingMatrix): string => {
   let scssContent = 'body:not(.is-tight) {\n';
+  let mobileContent = '';
 
   for (const entry of spacingMatrix) {
-    const [insertType, component, component2, spacingValue] = entry;
+    const [insertType, component, component2, spacingValue, mobileSpacingValue] = entry;
     let selector = '';
-    const spacingVal = spacingValue as SpacerSize || component2 as SpacerSize;
 
     if (insertType === INSERT_SPACE_BETWEEN) {
-      if (!component2) {
-        // Si une seule composante est donnée, générer une règle simple
+      if (!component2 || typeof component2 === 'number') {
         selector = `.${component.toLowerCase()}`;
       } else {
-        if (typeof component === 'string' && component2 === 'default') {
-          selector = `.${component.toLowerCase()} + *`;
-        } else if (component === 'default' && typeof component2 === 'string') {
+        if (component === 'default' && typeof component2 === 'string') {
           selector = `* + .${component2.toLowerCase()}`;
+        } else if (typeof component === 'string' && component2 === 'default') {
+          selector = `.${component.toLowerCase()}:not(:first-child) + *`;
         } else if (typeof component === 'string' && typeof component2 === 'string') {
           selector = `.${component.toLowerCase()} + .${component2.toLowerCase()}`;
-        } else if (typeof component === 'string' && typeof component2 === 'number') {
-          selector = `.${component.toLowerCase()}`;
         }
       }
 
-      const marginProperty = `margin-top`;
-
-      console.log(`Processing: ${selector}, ${marginProperty}, ${getSpacingValue(spacingVal)}px`);
+      const spacingVal = spacingValue as SpacerSize;
+      const mobileSpacingVal = mobileSpacingValue as SpacerSize | undefined;
 
       scssContent += `  ${selector} {\n`;
-      scssContent += `    ${marginProperty}: ${getSpacingValue(spacingVal)}px;\n`;
+      scssContent += `    margin-top: ${getSpacingValue(spacingVal)}px;\n`;
       scssContent += '  }\n';
+
+      console.log('mobileSpacingVal', mobileSpacingVal)
+
+      if (mobileSpacingVal !== undefined) {
+        mobileContent += `  ${selector} {\n`;
+        mobileContent += `    margin-top: ${getSpacingValue(mobileSpacingVal)}px;\n`;
+        mobileContent += '  }\n';
+      }
     }
   }
 
   scssContent += `}\n`;
 
-  return scssContent;
+  if (mobileContent) {
+    scssContent += `
+  @media (max-width: 768px) {
+    body:not(.is-tight) {
+    ${mobileContent}
+  }
 }
+`;
+  }
 
-const getSpacingValue = (spacingValue: any): number => {
+  return scssContent;
+};
+
+const getSpacingValue = (spacingValue: SpacerSize): number => {
   switch (spacingValue) {
     case THREE:
       return 8;
@@ -75,13 +88,13 @@ const getSpacingValue = (spacingValue: any): number => {
     default:
       return 8;
   }
-}
+};
 
 const scssContent = createBodyAutolayoutSCSS(DEFAULT_SPACING_MATRIX);
 
 const scssFilePath = './framework/src/components/_autolayout.scss';
 
-fs.writeFile(scssFilePath, scssContent, (err) => {
+fs.writeFile(scssFilePath, scssContent, err => {
   if (err) {
     console.error('Error writing SCSS file:', err);
   } else {
