a8ba39a458b2913d9ecb169a7c729213c6aab420
erwan <exavierdegetel@gmail.com>
2024-07-16 16:39:50 +0200

    Add attributes for accessible modal

--- examples/react-template/screens/Modal.tsx
+++ examples/react-template/screens/Modal.tsx
@@ -95,9 +95,9 @@ export const ModalScreen = (): JSX.Element => {
             "\n" +
             'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.'
           }
-          triggerMarkup={ModalMarkup.A}
+          triggerMarkup={ModalMarkup.BUTTON}
           title="title modal"
-          triggerContent="Open modal"
+          triggerContent="Open accessible modal"
           ctaContent="Action"
           ctaCancelOnClick={() => alert("cancel action")}
           // eslint-disable-next-line no-alert
--- packages/react/components/modal/title/ModalTitle.tsx
+++ packages/react/components/modal/title/ModalTitle.tsx
@@ -4,6 +4,7 @@ import { ModalTitleProps } from "./ModalTitleProps"
 import { hashClass } from "@/helpers"
 import clsx from "clsx"
 import { useTrilogyContext } from "@/context"
+import TextIcon from "@/components/icon/text/TextIcon"
 
 /**
  * Modal Title Component
@@ -16,19 +17,22 @@ const ModalTitle = ({
   children,
   className,
   iconColor,
-  iconName
+  iconName,
+  ...others
 }: ModalTitleProps): JSX.Element => {
   const { styled } = useTrilogyContext()
+  const {textId} = others as any
 
   return (
     <div className={hashClass(styled, clsx("modal-title", className))}>
       {iconName ? (
-        <Icon
+        <TextIcon
           color={iconColor}
           size={"large"}
           name={iconName}
           content={children}
           position='up'
+          textId={textId}
         />
       ) : (
         children
