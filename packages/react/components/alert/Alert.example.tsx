3a7f4d77e8bf5ad4748bee19b4aa19603207650a
Thomas Lopez <thlopez@bouyguestelecom.fr>
2024-07-24 10:57:19 +0200

    component file example

---
+++ packages/react/components/alert/Alert.example.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Alert } from './index'
+
+const AlertExample: React.ReactNode = <Alert
+  alert='INFO'
+  description='Lorem Ipsum is simply dummy text type and scrambled it to make a type specimen book..'
+  display
+  title='Alert information'
+/>
+
+export default AlertExample
