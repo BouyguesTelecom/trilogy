319ecb721e13e12581406afbad2398307de237e6
erwan <exavierdegetel@gmail.com>
2024-08-07 10:46:36 +0200

    clean section and add gapless props to rows

--- packages/react/components/rows/Rows.tsx
+++ packages/react/components/rows/Rows.tsx
@@ -3,17 +3,19 @@ import { RowsProps } from "./RowsProps"
 import { hashClass } from "@/helpers"
 import clsx from "clsx"
 import { useTrilogyContext } from "@/context"
+import { is } from "@/services"
 
 /**
  * Rows Component
  * @param children {React.ReactNode} Rows children
+ * @param gapless {boolean} Delete margins between row
  * - ------------------- WEB PROPERTIES -------------------------
  * @param className {string} additionnal CSS Classes
  */
-const Rows = ({ className, ...others }: RowsProps): JSX.Element => {
+const Rows = ({ className, gapless, ...others }: RowsProps): JSX.Element => {
   const { styled } = useTrilogyContext()
   return (
-    <div className={hashClass(styled, clsx("rows", className))} {...others} />
+    <div className={hashClass(styled, clsx("rows", gapless && is("gapless"), className))} {...others} />
   )
 }
 
