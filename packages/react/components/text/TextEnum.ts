a83d3d1b86efe3cce07aee4b97ee41704abd52d4
JulienMora <julien.moratalla@gmail.com>
2024-09-25 10:25:18 +0200

    Rename enum / values for levels

--- packages/react/components/text/TextEnum.ts
+++ packages/react/components/text/TextEnum.ts
@@ -2,10 +2,10 @@
  * New Text Levels
  */
 export enum TextLevels {
-  ONE = 'ONE',
-  TWO = 'TWO',
-  THREE = 'THREE',
-  FOUR = 'FOUR',
+  ONE = 1,
+  TWO = 2,
+  THREE = 3,
+  FOUR = 4,
 }
 
 /**
--- packages/react/components/title/TitleEnum.ts
+++ packages/react/components/title/TitleEnum.ts
@@ -2,12 +2,12 @@
  * New Title Levels
  */
 export enum TitleLevels {
-  ONE = 'ONE',
-  TWO = 'TWO',
-  THREE = 'THREE',
-  FOUR = 'FOUR',
-  FIVE = 'FIVE',
-  SIX = 'SIX',
+  ONE = 1,
+  TWO = 2,
+  THREE = 3,
+  FOUR = 4,
+  FIVE = 5,
+  SIX = 6,
 }
 
 /**
