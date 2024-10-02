27b6223fd5857ef116ae8899f12c2192cf935101
JulienMora <julien.moratalla@gmail.com>
2024-10-02 10:15:19 +0200

    Fix tests

--- packages/react/components/spacer/test/Spacer.test.tsx
+++ packages/react/components/spacer/test/Spacer.test.tsx
@@ -5,18 +5,18 @@ import { SpacerSize } from "../SpacerEnum";
 
 describe("Spacer component", () => {
   it("renders the spacer with default props", () => {
-    const { container } = render(<Spacer size={SpacerSize.HUGE} />);
+    const { container } = render(<Spacer size={SpacerSize.SEVEN} />);
     expect(container.firstChild).toHaveStyle("marginLeft: 0px");
   });
 
   it("renders the spacer with horizontal margin", () => {
-    const { container } = render(<Spacer horizontal size={SpacerSize.SMALL} />);
+    const { container } = render(<Spacer horizontal size={SpacerSize.TWO} />);
     expect(container.firstChild).toHaveStyle("marginLeft: 8px");
     expect(container.firstChild).toHaveStyle("marginTop: 0px");
   });
 
   it("renders the spacer with vertical margin", () => {
-    const { container } = render(<Spacer size={SpacerSize.LARGE} />);
+    const { container } = render(<Spacer size={SpacerSize.FIVE} />);
     expect(container.firstChild).toHaveStyle("marginLeft: 0px");
     expect(container.firstChild).toHaveStyle("marginTop: 24px");
   });
