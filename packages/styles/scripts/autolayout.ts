d8e34faf2666ad69330a58af26377f9d0aaf23cb
JulienMora <julien.moratalla@gmail.com>
2024-10-08 15:52:30 +0200

    Update autolayout rules

--- packages/styles/scripts/autolayout.ts
+++ packages/styles/scripts/autolayout.ts
@@ -5,7 +5,7 @@ import { SpacingMatrixMode } from "@trilogy-ds/react/components/autolayout/Spaci
 
 type DefaultSpacingMatrix = Array<[SpacingMatrixMode, string, (string | SpacerSize)?, SpacerSize?, SpacerSize?]>;
 
-const { THREE, FOUR, FIVE, TWO, SEVEN, ZERO, SIX } = SpacerSize;
+const { THREE, FOUR, FIVE, TWO, ONE, SEVEN, ZERO, SIX } = SpacerSize;
 
 /*
 * @first component: string (can not be default)
@@ -31,7 +31,7 @@ export const DEFAULT_SPACING_MATRIX: DefaultSpacingMatrix = [
   [INSERT_SPACE_BETWEEN, '.countdown', 'default', FOUR, THREE],
   [INSERT_SPACE_BETWEEN, '.countdown.is-small', 'default', THREE, THREE],
   [INSERT_SPACE_BETWEEN, '.chips-list', 'default', THREE, THREE],
-  [INSERT_SPACE_BETWEEN, 'p.text:not(:last-child)', 'default', THREE, TWO],
+  [INSERT_SPACE_BETWEEN, 'p.text', 'default', THREE, TWO],
   [INSERT_SPACE_BETWEEN, '.Progress', 'default', TWO, TWO],
   [INSERT_SPACE_BETWEEN, '.progress-radial', 'default', TWO, TWO],
   [INSERT_SPACE_BETWEEN, '.Radio', 'default', THREE, THREE],
@@ -40,17 +40,20 @@ export const DEFAULT_SPACING_MATRIX: DefaultSpacingMatrix = [
   [INSERT_SPACE_BETWEEN, '.radio-tile', 'default', FIVE, FOUR],
   [INSERT_SPACE_BETWEEN, '.checkbox-tile', 'default', FIVE, FOUR],
   [INSERT_SPACE_BETWEEN, '.input', 'default', FOUR, FOUR],
+  [INSERT_SPACE_BETWEEN, '.field', 'default', FIVE, FOUR],
+  [INSERT_SPACE_BETWEEN, '.control', 'default', ONE, ONE],
+  [INSERT_SPACE_BETWEEN, '.help', 'default', ONE, ONE],
   [INSERT_SPACE_BETWEEN, '.textarea', 'default', FOUR, FOUR],
   [INSERT_SPACE_BETWEEN, '.select', 'default', FOUR, FOUR],
   [INSERT_SPACE_BETWEEN, '.link', 'default', THREE, TWO],
   [INSERT_SPACE_BETWEEN, '.sticker', 'default', TWO, TWO],
   [INSERT_SPACE_BETWEEN, '.tag', 'default', TWO, TWO],
-  [INSERT_SPACE_BETWEEN, '.title.is-level-1:not(:last-child)', 'default', SEVEN, SIX],
-  [INSERT_SPACE_BETWEEN, '.title.is-level-2:not(:last-child)', 'default', SIX, FIVE],
-  [INSERT_SPACE_BETWEEN, '.title.is-level-3:not(:last-child)', 'default', FOUR, THREE],
-  [INSERT_SPACE_BETWEEN, '.title.is-level-4:not(:last-child)', 'default', THREE, TWO],
-  [INSERT_SPACE_BETWEEN, '.title.is-level-5:not(:last-child)', 'default', THREE, TWO],
-  [INSERT_SPACE_BETWEEN, '.title.is-level-6:not(:last-child)', 'default', THREE, TWO],
+  [INSERT_SPACE_BETWEEN, '.title.is-level-1', 'default', SEVEN, SIX],
+  [INSERT_SPACE_BETWEEN, '.title.is-level-2', 'default', SIX, FIVE],
+  [INSERT_SPACE_BETWEEN, '.title.is-level-3', 'default', FOUR, THREE],
+  [INSERT_SPACE_BETWEEN, '.title.is-level-4', 'default', THREE, TWO],
+  [INSERT_SPACE_BETWEEN, '.title.is-level-5', 'default', THREE, TWO],
+  [INSERT_SPACE_BETWEEN, '.title.is-level-6', 'default', THREE, TWO],
   [INSERT_SPACE_BETWEEN, '.overline', 'default', ZERO, ZERO],
   [INSERT_SPACE_BETWEEN, '.title.is-level-1', '.subtitle', TWO, TWO],
   [INSERT_SPACE_BETWEEN, '.title.is-level-2', '.subtitle', TWO, TWO],
@@ -81,12 +84,12 @@ const createBodyAutolayoutSCSS = (spacingMatrix: DefaultSpacingMatrix): string =
       const spacingVal = spacingValue as SpacerSize;
       const mobileSpacingVal = mobileSpacingValue as SpacerSize | undefined;
 
-      scssContent += `  ${selector} {\n`;
+      scssContent += `  ${selector}:not(:last-child) {\n`;
       scssContent += `    margin-bottom: ${spacingVal}px;\n`;
       scssContent += '  }\n';
 
       if (mobileSpacingVal !== undefined) {
-        mobileContent += `  ${selector} {\n`;
+        mobileContent += `  ${selector}:not(:last-child) {\n`;
         mobileContent += `      margin-bottom: ${mobileSpacingVal}px;\n`;
         mobileContent += '    }\n';
       }
