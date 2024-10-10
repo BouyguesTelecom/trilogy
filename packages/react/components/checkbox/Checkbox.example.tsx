38d37db21e0f90ae15a486b3d0708212892c8a66
Eliott <ecormill@bouyguestelecom.fr>
2024-10-10 16:17:54 +0200

    more example for alert, button, checkbox, box

--- packages/react/components/checkbox/Checkbox.example.tsx
+++ packages/react/components/checkbox/Checkbox.example.tsx
@@ -1,24 +1,81 @@
 import React from 'react'
 import Checkbox from '@/components/checkbox/Checkbox'
+import { Columns, ColumnsItem } from '../columns'
 
 const CheckboxExample: React.ReactNode = <div>
-  <Checkbox
-    label="On peut me cocher grâce au controls ↓ "
-    name="checkbox1"
-    value="default value 1"
-  />
-  <Checkbox
-    checked
-    label="Je suis cochée"
-    name="checkbox2"
-    value="default value 2"
-  />
-  <Checkbox
-    disabled
-    label="Je suis disabled"
-    name="checkbox3"
-    value="default value 3"
-  />
+            <>
+                <Columns multiline>
+                    <ColumnsItem size={4}>
+                        <Checkbox
+                            label="On peut me cocher grâce au controls ↓ "
+                            name="checkbox1"
+                            onChange={function noRefCheck() { }}
+                            onClick={function noRefCheck() { }}
+                            value="default value 1"
+                        />
+                    </ColumnsItem>
+
+                    <ColumnsItem size={4}>
+                        <Checkbox
+                            checked
+                            label="Je suis cochée"
+                            name="checkbox2"
+                            onClick={function noRefCheck() { }}
+                            value="default value 2"
+                        />
+                    </ColumnsItem>
+
+                    <ColumnsItem size={4}>
+                        <Checkbox
+                            disabled
+                            label="Je suis disabled"
+                            name="checkbox3"
+                            onClick={function noRefCheck() { }}
+                            value="default value 3"
+                        />
+                    </ColumnsItem>
+                    <ColumnsItem size={4}>
+                    <Checkbox
+                        description="On peut me cocher et ajouter une icône grâce au controls ↓ "
+                        horizontalTile
+                        iconTile="tri-check-circle"
+                        label="Checkbox tile horizontal"
+                        name="checkbox"
+                        onChange={function noRefCheck() { }}
+                        onClick={function noRefCheck() { }}
+                        tile
+                        value="default value 1"
+                    />
+                    </ColumnsItem>
+                    <ColumnsItem size={4}>
+                    <Checkbox
+                        checked
+                        description="Je suis la description de la checkbox"
+                        horizontalTile
+                        iconTile="tri-infos-circle"
+                        label="Checkbox tile avec Icône"
+                        name="checkbox"
+                        onClick={function noRefCheck() { }}
+                        tile
+                        value="default value 1"
+                    />
+                    </ColumnsItem>
+                    <ColumnsItem size={4}>
+                    <Checkbox
+                        description="Je suis la description de la checkbox"
+                        disabled
+                        horizontalTile
+                        iconTile="tri-watch"
+                        label="Checkbox tile horizontal disabled"
+                        name="checkbox"
+                        onClick={function noRefCheck() { }}
+                        tile
+                        value="default value 1"
+                    />
+                    </ColumnsItem>
+                </Columns>
+
+            </>
 </div>
 
 export default CheckboxExample
