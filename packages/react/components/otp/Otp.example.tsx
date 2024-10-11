04efa8f22efa663d4b1f219a39360c9516d03400
Eliott <ecormill@bouyguestelecom.fr>
2024-10-11 19:26:53 +0200

    add more examples Countdown/Fab/List/Otp

--- packages/react/components/otp/Otp.example.tsx
+++ packages/react/components/otp/Otp.example.tsx
@@ -1,11 +1,40 @@
 import React from 'react'
 import { Otp } from './index'
+import { Columns, ColumnsItem } from '../columns'
 
 const OtpExample: React.ReactNode =
   <div>
-    <Otp
-      label="Saisir votre otp"
-    />
+            <>
+                <Columns>
+                    <ColumnsItem size={4}>
+                        <Otp
+                            error
+                            errorMessage="Ceci est un message  derreur"
+                            label="Saisir votre otp"
+                            onChange={function noRefCheck() { }}
+                            onCompleted={function noRefCheck() { }}
+                            onFocus={function noRefCheck() { }}
+                        />
+                        </ColumnsItem>
+                        <ColumnsItem size={4}>
+                        <Otp
+                            label="Saisir votre otp"
+                            onChange={function noRefCheck() { }}
+                            onCompleted={function noRefCheck() { }}
+                            onFocus={function noRefCheck() { }}
+                        />
+                        </ColumnsItem>
+                    <ColumnsItem size={4}>
+                        <Otp
+                            disabled
+                            label="Saisir votre otp"
+                            onChange={function noRefCheck() { }}
+                            onCompleted={function noRefCheck() { }}
+                            onFocus={function noRefCheck() { }}
+                        />
+                    </ColumnsItem>
+                </Columns>
+            </>
   </div>
 
 export default OtpExample
