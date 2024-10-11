365eae91c1f041c75cbb34d3df1830b9bef51c9b
Eliott <ecormill@bouyguestelecom.fr>
2024-10-11 19:26:53 +0200

    add more examples Countdown/Fab/List/Otp

--- packages/react/components/fab/Fab.example.tsx
+++ packages/react/components/fab/Fab.example.tsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import { Fab } from './index'
+import { Columns, ColumnsItem } from '../columns';
 
 
-const FabExample: React.ReactNode = <Fab
-  iconName='tri-alert'
->
-  Ecrire
-</Fab>
+const FabExample: React.ReactNode = 
+
+<>
+<Columns>
+<ColumnsItem>
+    <Fab
+        iconName="tri-check"
+        onClick={function noRefCheck() { }}
+    >
+        Ecrire
+    </Fab>
+    </ColumnsItem>
+    <ColumnsItem>
+    <Fab
+        extended
+        iconName="tri-bell"
+        onClick={function noRefCheck() { }}
+    >
+        Extended fab
+    </Fab>
+    </ColumnsItem>
+    </Columns>
+</>
+
 export default FabExample;
