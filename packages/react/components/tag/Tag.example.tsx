8345a5cd60827532964ab89a5f979b21ebf89ebd
Eliott <ecormill@bouyguestelecom.fr>
2024-10-15 10:51:30 +0200

    fix tag example

--- packages/react/components/tag/Tag.example.tsx
+++ packages/react/components/tag/Tag.example.tsx
@@ -1,32 +1,27 @@
 import React from 'react'
 import { Tag, TagList } from './index'
 import { Columns, ColumnsItem } from '../columns'
+import { IconName } from '@/components/icon'
 
-const TagExample: React.ReactNode =
-
-<Columns>
+const TagExample: React.ReactNode = (
+  <Columns>
     <ColumnsItem>
-        <TagList>
-            <Tag
-                iconName='tri-alert'
-                variant='ERROR'
-            >
-                Tag
-            </Tag>
-            <Tag
-                iconName='tri-bell'
-                variant='SUCCESS'
-            >
-                Tag success
-            </Tag>
-            <Tag
-                iconName='tri-check'
-                variant='WARNING'
-            >
-                Tag warning
-            </Tag>
-        </TagList>
+      <TagList>
+        <Tag iconName={IconName.BELL} variant='ERROR'>
+          Tag
+        </Tag>
+        <Tag iconName={IconName.ALERT} variant='SUCCESS'>
+          Tag success
+        </Tag>
+        <Tag iconName={IconName.CHECK} variant='WARNING'>
+          Tag warning
+        </Tag>
+        <Tag iconName={IconName.EYE} variant='INFO'>
+          Tag info
+        </Tag>
+      </TagList>
     </ColumnsItem>
-</Columns>
+  </Columns>
+)
 
 export default TagExample
