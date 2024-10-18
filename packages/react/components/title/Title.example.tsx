d2aa4e3f16003191cd88982e0ae5b411830ce5d4
Eliott <ecormill@bouyguestelecom.fr>
2024-10-18 10:49:28 +0200

    more examples

--- packages/react/components/title/Title.example.tsx
+++ packages/react/components/title/Title.example.tsx
@@ -2,24 +2,15 @@ import React from 'react'
 import { Columns, ColumnsItem } from '../columns'
 import { Title } from './index'
 
-const TitleExample: React.ReactNode =
-
-  <>
-    <Columns multiline>
-      <ColumnsItem size={12}>
-        <Title
-          level="ONE"
-          onClick={function noRefCheck() { }}
-        >
-          Ceci titre
-        </Title>
-      </ColumnsItem>
-      <ColumnsItem size={12}>
-        <Title subtitle>
-          Ceci est un sous-titre
-        </Title>
-      </ColumnsItem>
-    </Columns>
-  </>
+const TitleExample: React.ReactNode = (
+  <Columns multiline>
+    <ColumnsItem size={12}>
+      <Title level='ONE'>Ceci titre</Title>
+    </ColumnsItem>
+    <ColumnsItem size={12}>
+      <Title subtitle>Ceci est un sous-titre</Title>
+    </ColumnsItem>
+  </Columns>
+)
 
 export default TitleExample
