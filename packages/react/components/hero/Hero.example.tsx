ac6306b603e89bad5eb48b11629b8db759b7a076
Eliott <ecormill@bouyguestelecom.fr>
2024-10-18 10:57:07 +0200

    fix hero

--- packages/react/components/hero/Hero.example.tsx
+++ packages/react/components/hero/Hero.example.tsx
@@ -2,39 +2,35 @@ import React from 'react'
 import { Hero } from './index'
 import { Container } from '@/components/container'
 import { Title } from '@/components/title'
-import { Box, Columns, ColumnsItem, Section, Text } from '@/lib'
+import { Columns, ColumnsItem } from '@/components/columns'
+import { Section } from '@/components/section'
+import { Box, BoxContent } from '@/components/box'
+import { Text } from '@/components/text'
 
 const HeroExample: React.ReactNode = (
   <Columns multiline>
-      <ColumnsItem>
-        <Hero overlap className='has-pattern-dark'>
-          <Container>
-            <Title
-              inverted
-              level='TWO'
-              markup='h1'
-            >
-              Internet garanti
-            </Title>
-            <Text
-              className='is-inverted has-text-weight-bold'
-              inverted
-            >
-              Profitez dInternet dès labonnement et même en cas de coupure grâce à une clé 4G dans
-              les nouvelles offres Bbox.
-            </Text>
-          </Container>
-        </Hero>
-        <Section>
-          <Container>
-            <Box>
-              <Title level='TWO'>
-                Internet garanti
-              </Title>
-            </Box>
-          </Container>
-        </Section>
-      </ColumnsItem>
-    </Columns>
+    <ColumnsItem>
+      <Hero overlap className='has-pattern-dark'>
+        <Container>
+          <Title inverted level='TWO' markup='h1'>
+            Internet garanti
+          </Title>
+          <Text className='is-inverted has-text-weight-bold' inverted>
+            Profitez dInternet dès labonnement et même en cas de coupure grâce à une clé 4G dans les nouvelles offres
+            Bbox.
+          </Text>
+        </Container>
+      </Hero>
+      <Section>
+        <Container>
+          <Box>
+            <BoxContent>
+              <Title level='TWO'>Internet garanti</Title>
+            </BoxContent>
+          </Box>
+        </Container>
+      </Section>
+    </ColumnsItem>
+  </Columns>
 )
 export default HeroExample
