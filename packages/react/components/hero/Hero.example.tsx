77f50f8528db2a07a510b38075c8a927ba8e1f39
Eliott <ecormill@bouyguestelecom.fr>
2024-10-18 10:49:28 +0200

    more examples

--- packages/react/components/hero/Hero.example.tsx
+++ packages/react/components/hero/Hero.example.tsx
@@ -2,31 +2,39 @@ import React from 'react'
 import { Hero } from './index'
 import { Container } from '@/components/container'
 import { Title } from '@/components/title'
-import { Button } from '@/components/button'
+import { Box, Columns, ColumnsItem, Section, Text } from '@/lib'
 
-const HeroExample: React.ReactNode = <Hero
-  backgroundSrc='https://picsum.photos/id/1/1500/600'
-
->
-  <Container>
-    <Title
-      inverted
-      level='TWO'
-      markup='h1'
-    >
-      Bonjour Michel
-    </Title>
-    <Title
-      inverted
-      markup='span'
-      subtitle
-    >
-      Fugiat velit dolor ad adipisicing id quis enim cupidatat Lorem dolore aute excepteur tempor.
-    </Title>
-    <Button>
-      Click me !{' '}
-    </Button>
-  </Container>
-</Hero>
-
+const HeroExample: React.ReactNode = (
+  <Columns multiline>
+      <ColumnsItem>
+        <Hero overlap className='has-pattern-dark'>
+          <Container>
+            <Title
+              inverted
+              level='TWO'
+              markup='h1'
+            >
+              Internet garanti
+            </Title>
+            <Text
+              className='is-inverted has-text-weight-bold'
+              inverted
+            >
+              Profitez dInternet dès labonnement et même en cas de coupure grâce à une clé 4G dans
+              les nouvelles offres Bbox.
+            </Text>
+          </Container>
+        </Hero>
+        <Section>
+          <Container>
+            <Box>
+              <Title level='TWO'>
+                Internet garanti
+              </Title>
+            </Box>
+          </Container>
+        </Section>
+      </ColumnsItem>
+    </Columns>
+)
 export default HeroExample
