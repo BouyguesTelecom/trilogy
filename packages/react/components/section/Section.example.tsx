c69d9c638510f1b74fde90a3854d7e514807f844
Eliott <ecormill@bouyguestelecom.fr>
2024-10-23 09:48:46 +0200

    fix section

--- packages/react/components/section/Section.example.tsx
+++ packages/react/components/section/Section.example.tsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import { Section } from './index'
 import { Title } from '@/components/title'
+import { Columns, ColumnsItem } from '@/lib'
 
-const SectionExample: React.ReactNode =
-
-  <>
-    <Section>
-      <Title level='ONE'>
-        Première section
-      </Title>
-    </Section>
-    <Section>
-      <Title level='ONE'>
-        Deuxième section
-      </Title>
-    </Section>
-    <Section>
-      <Title level='ONE'>
-        Troisième section
-      </Title>
-    </Section>
-  </>
-
+const SectionExample: React.ReactNode = (
+  <Columns>
+    <ColumnsItem>
+      <Section>
+        <Title level='ONE'>Première section</Title>
+      </Section>
+    </ColumnsItem>
+    <ColumnsItem>
+      <Section>
+        <Title level='ONE'>Deuxième section</Title>
+      </Section>
+    </ColumnsItem>
+    <ColumnsItem>
+      <Section>
+        <Title level='ONE'>Troisième section</Title>
+      </Section>
+    </ColumnsItem>
+  </Columns>
+)
 export default SectionExample
