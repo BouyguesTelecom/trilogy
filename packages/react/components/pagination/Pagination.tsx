d1ec42c1a6d585ea6bbac0edea577096afa434ff
Weckr Stephane <swecker@bouyguestelecom.fr>
2024-10-24 11:21:12 +0200

    Pagination on mobile in one line

--- packages/react/components/pagination/Pagination.tsx
+++ packages/react/components/pagination/Pagination.tsx
@@ -48,13 +48,13 @@ const Pagination = ({
       // more than 3 total pages so calculate start and end pages
       if (currentPage <= 3) {
         startPage = 1
-        endPage = 5
+        endPage = 4
       } else if (currentPage + 3 >= totalPages) {
-        startPage = totalPages - 4
+        startPage = totalPages - 3
         endPage = totalPages
       } else {
-        startPage = currentPage - 2
-        endPage = currentPage + 2
+        startPage = currentPage - 1
+        endPage = currentPage + 1
       }
     }
 
