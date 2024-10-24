dd9287f40ea82be9328cab48df76cb0ab65408f1
Weckr Stephane <swecker@bouyguestelecom.fr>
2024-10-24 10:12:57 +0200

    Badge Inverted

--- packages/react/components/badge/BadgeProps.ts
+++ packages/react/components/badge/BadgeProps.ts
@@ -1,11 +1,12 @@
 import { Clickable } from '@/objects/facets/Clickable'
+import { Invertable } from '@/objects/facets/Invertable'
 import { BadgeColor, BadgeColorValues } from './BadgeEnum'
 import { Accessibility } from '@/objects'
 
 /**
  * Badge Interface
  */
-export interface BadgeProps extends Clickable, Accessibility {
+export interface BadgeProps extends Clickable, Accessibility, Invertable {
   children?: React.ReactNode
   content?: string | number
   textContent?: string
