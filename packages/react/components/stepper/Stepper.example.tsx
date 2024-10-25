c7d628eca6ec9694fb1ed6459bebc2f7dbc47bc1
Eliott <ecormill@bouyguestelecom.fr>
2024-10-25 10:02:23 +0200

    fix example

--- packages/react/components/stepper/Stepper.example.tsx
+++ packages/react/components/stepper/Stepper.example.tsx
@@ -1,33 +1,14 @@
 import React from 'react'
 import { Stepper, StepperStep } from './index'
 
-const StepperExample: React.ReactNode =
+const StepperExample: React.ReactNode = (
   <Stepper centered>
-    <StepperStep
-      done
-      label='Récapitulatif'
-      step={1}
-    />
-    <StepperStep
-      done
-      label='Compléments'
-      step={2}
-    />
-    <StepperStep
-      done
-      label='Coordonnées'
-      step={3}
-    />
-    <StepperStep
-      current
-      label='Livraison'
-      step={4}
-    />
-    <StepperStep
-      label='Confirmation'
-      step={5}
-    />
-    Stepp
+    <StepperStep done label='Récapitulatif' step={1} />
+    <StepperStep done label='Compléments' step={2} />
+    <StepperStep current label='Coordonnées' step={3} />
+    <StepperStep label='Livraison' step={4} />
+    <StepperStep label='Confirmation' step={5} />
   </Stepper>
+)
 
 export default StepperExample
