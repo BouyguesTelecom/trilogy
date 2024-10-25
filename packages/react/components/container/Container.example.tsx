b67fd64acf8905bd6c2401a894ac48c3a26a7704
Eliott <ecormill@bouyguestelecom.fr>
2024-10-25 10:02:23 +0200

    fix example

--- packages/react/components/container/Container.example.tsx
+++ packages/react/components/container/Container.example.tsx
@@ -4,13 +4,12 @@ import { Box } from '@/components/box'
 import { Text } from '@/components/text'
 import { Container } from './index'
 
-const ContainerExample: React.ReactNode =
-<Container>
-  <Box>
-    <Text>
-      Je suis une box dans un container
-    </Text>
-  </Box>
-</Container>
+const ContainerExample: React.ReactNode = (
+  <Container>
+    <Box>
+      <Text>Je suis une box dans un container</Text>
+    </Box>
+  </Container>
+)
 
 export default ContainerExample
