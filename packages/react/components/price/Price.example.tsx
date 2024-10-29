29437b6ae44f1792cfba8caa870a348c4de3b8f1
Paul Naszalyi <pnaszaly@bouyguestelecom.fr>
2024-10-29 15:54:04 +0100

    Fix Price exampel

--- packages/react/components/price/Price.example.tsx
+++ packages/react/components/price/Price.example.tsx
@@ -2,34 +2,18 @@ import React from 'react'
 import { Price } from './index'
 import { Columns, ColumnsItem } from '../columns'
 
-const PriceExample: React.ReactNode =
-
-<Columns>
+const PriceExample: React.ReactNode = (
+  <Columns>
     <ColumnsItem>
-        <Price
-            align='ALIGNED_START'
-            amount={24.99}
-            period='mois'
-            showCents
-        />
+      <Price align='ALIGNED_START' amount={24.99} period='mois' showCents />
     </ColumnsItem>
     <ColumnsItem>
-        <Price
-            amount={24.99}
-            mention='(1)'
-            period='mois'
-            showCents
-            strikedAmount={29.99}
-        />
+      <Price amount={24.99} mention='(1)' period='mois' showCents strikedAmount={29.99} />
     </ColumnsItem>
     <ColumnsItem>
-        <Price
-            amount={24.99}
-            mention='(1)'
-            period='mois'
-            showCents
-        />
+      <Price amount={24.99} mention='(1)' period='mois' showCents />
     </ColumnsItem>
-</Columns>
+  </Columns>
+)
 
 export default PriceExample
