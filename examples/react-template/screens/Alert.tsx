ea1fd46f36955c97a4eada16bf49b420a58a1572
Weckr Stephane <swecker@bouyguestelecom.fr>
2024-10-29 10:41:10 +0100

    Adding Alert Banner

--- examples/react-template/screens/Alert.tsx
+++ examples/react-template/screens/Alert.tsx
@@ -59,6 +59,13 @@ export const AlertScreen = (): JSX.Element => {
 
   return (
     <Section>
+      <Alert
+        banner
+        status={StatusState.INFO}
+        title="Banner Alert"
+        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.."
+      />
+
     <ToasterAlertProvider>
       <ToasterAlertView />
       <Container>
