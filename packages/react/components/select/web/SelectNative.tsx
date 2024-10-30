68b4f3d4a046cd0e8c66aa5939c28d9fa08d91bb
erwan <exavierdegetel@gmail.com>
2024-10-30 18:54:55 +0100

    Delete html icon and update scss

--- packages/react/components/select/web/SelectNative.tsx
+++ packages/react/components/select/web/SelectNative.tsx
@@ -1,12 +1,12 @@
 import clsx from 'clsx'
 import * as React from 'react'
 
-import { hashClass } from '@/helpers/hashClassesHelpers'
-import { has, is } from '@/services/classify'
-import { Icon, IconSize } from '@/components/icon'
-import { ParamEventSelectFocus, SelectProps } from '@/components/select/SelectProps'
+import { Icon } from '@/components/icon'
 import { SelectOption } from '@/components/select'
+import { ParamEventSelectFocus, SelectProps } from '@/components/select/SelectProps'
 import { useTrilogyContext } from '@/context/index'
+import { hashClass } from '@/helpers/hashClassesHelpers'
+import { has } from '@/services/classify'
 
 const SelectNative = ({
   onChange,
@@ -26,26 +26,16 @@ const SelectNative = ({
   ...others
 }: SelectProps): JSX.Element => {
   const { styled } = useTrilogyContext()
-  const selectClasses = React.useMemo(() => hashClass(styled, clsx('select', className)), [styled, className, iconName])
+  const refSelect = React.useRef<HTMLSelectElement>(null)
+
   const [focused, setIsFocused] = React.useState<boolean>(false)
   const [selectedValues, setSelectedValues] = React.useState(selected)
 
-  React.useEffect(() => {
-    setSelectedValues(selected)
-  }, [selected])
+  const selectClasses = React.useMemo(() => hashClass(styled, clsx('select', className)), [styled, className])
 
-  const classes = React.useMemo(
-    () => ({
-      input: hashClass(
-        styled,
-        clsx('input', disabled && is('disabled'), 'select-native', multiple && 'multiple', className),
-      ),
-      control: hashClass(
-        styled,
-        clsx('control', has('dynamic-placeholder'), iconName && 'has-icons-left', iconName && 'has-icons-right'),
-      ),
-    }),
-    [styled, disabled, iconName, className, multiple],
+  const controlClass = React.useMemo(
+    () => hashClass(styled, clsx('control', has('dynamic-placeholder'), iconName && 'has-icons-left')),
+    [styled, iconName],
   )
 
   const handleFocus = React.useCallback((e: ParamEventSelectFocus) => {
@@ -58,59 +48,51 @@ const SelectNative = ({
     onBlur && onBlur(e)
   }, [])
 
+  React.useEffect(() => {
+    setSelectedValues(selected)
+  }, [selected])
+
   return (
     <div className={selectClasses}>
       <div className={hashClass(styled, clsx('field', focused && 'focus'))}>
-        <div className={classes.control}>
-          <div className={classes.input}>
-            <select
-              multiple={multiple}
-              className={hashClass(styled, clsx(!label && 'no-label'))}
-              value={selectedValues}
-              aria-label={accessibilityLabel}
-              data-testid={testId}
-              onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
-                const selectedV = Array.from(e.target.selectedOptions).map((select) => select.value)
-                setSelectedValues(selectedV)
-                if (onChange) {
-                  onChange({
-                    selectValue: e.target.value,
-                    selectName: e.target.name,
-                    selectId: e.target.id,
-                    name: e.target.name,
-                    selectedOptions: selectedV,
-                  })
-                }
-              }}
-              onFocus={handleFocus}
-              onBlur={handleBlur}
-              id={id ? String(id) : undefined}
-              name={name}
-              disabled={disabled}
-              {...others}
-              role='listbox'
-            >
-              {React.Children.map(children, (child) => {
-                if (!React.isValidElement(child)) return null
-                const props = {
-                  ...child.props,
-                  native: 'true',
-                }
-                return <SelectOption {...props} />
-              })}
-            </select>
-          </div>
+        <div className={controlClass}>
+          <select
+            className={hashClass(styled, clsx(!label && 'no-label'))}
+            value={selectedValues}
+            aria-label={accessibilityLabel}
+            data-testid={testId}
+            onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+              const selectedV = Array.from(e.target.selectedOptions).map((select) => select.value)
+              setSelectedValues(selectedV)
+              if (onChange) {
+                onChange({
+                  selectValue: e.target.value,
+                  selectName: e.target.name,
+                  selectId: e.target.id,
+                  name: e.target.name,
+                  selectedOptions: selectedV,
+                })
+              }
+            }}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+            id={id ? String(id) : undefined}
+            name={name}
+            disabled={disabled}
+            role='listbox'
+            {...others}
+          >
+            {React.Children.map(children, (child) => {
+              if (!React.isValidElement(child)) return null
+              const props = {
+                ...child.props,
+                native: 'true',
+              }
+              return <SelectOption {...props} />
+            })}
+          </select>
           {label && !multiple && <label htmlFor={id}>{label}</label>}
-          {iconName && (
-            <div>
-              <Icon name={iconName} size='small' />
-            </div>
-          )}
-          {!multiple && (
-            <div>
-              <Icon className='icon-right' name={focused ? 'tri-arrow-up' : 'tri-arrow-down'} size={IconSize.SMALL} />
-            </div>
-          )}
+          {iconName && <Icon name={iconName} size='small' />}
         </div>
       </div>
     </div>
