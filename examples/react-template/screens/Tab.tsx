8076adb5ba0c6a3db405aa6d872b6e68da0a4c71
Paul Naszalyi <pnaszaly@bouyguestelecom.fr>
2024-11-18 18:06:16 +0100

    Fix align for Tabs

--- examples/react-template/screens/Tab.tsx
+++ examples/react-template/screens/Tab.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { Section, Tab, TabList, TabPanel, TabPanels, Tabs, Title, TitleLevels } from '@trilogy-ds/react/components'
-import { IconName } from '@trilogy-ds/react'
+import { Alignable, IconName } from '@trilogy-ds/react'
 
 export const TabScreen = (): JSX.Element => {
   return (
@@ -10,6 +10,32 @@ export const TabScreen = (): JSX.Element => {
 
       <Tabs>
         <TabList>
+          <Tab label='Tab 1' iconName={IconName.ALERT} />
+          <Tab label='Tab 2' iconName={IconName.ALERT} />
+          <Tab label='Tab 3' iconName={IconName.ALERT} />
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Title>Hello</Title>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+
+      <Tabs>
+        <TabList align={Alignable.ALIGNED_START}>
+          <Tab label='Tab 1' iconName={IconName.ALERT} />
+          <Tab label='Tab 2' iconName={IconName.ALERT} />
+          <Tab label='Tab 3' iconName={IconName.ALERT} />
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <Title>Hello</Title>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+
+      <Tabs>
+        <TabList align={Alignable.ALIGNED_END}>
           <Tab label='Tab 1' iconName={IconName.ALERT} />
           <Tab label='Tab 2' iconName={IconName.ALERT} />
           <Tab label='Tab 3' iconName={IconName.ALERT} />
--- packages/react/components/tabs/tab-list/TabList.tsx
+++ packages/react/components/tabs/tab-list/TabList.tsx
@@ -3,15 +3,17 @@ import { TabListProps } from './TabListProps'
 import { hashClass } from '@/helpers'
 import clsx from 'clsx'
 import { useTrilogyContext } from '@/context'
+import { getAlignClassName } from '@/objects'
+import { is } from '@/services'
 
 /**
  * Tabs Nav Component
  * @param children {ReactChild} React Child Element
  */
 const TabList = React.forwardRef((props: TabListProps) => {
-  const { children, className, id, testId, ...others } = props
+  const { children, className, id, testId, align, ...others } = props
   const { styled } = useTrilogyContext()
-  const classes = hashClass(styled, clsx('tab-list', className))
+  const classes = hashClass(styled, clsx('tab-list', align && is(getAlignClassName(align)), className))
 
   return (
     <div id={id} data-testid={testId} className={classes} {...others}>
