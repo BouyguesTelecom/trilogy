1f89f0deea6beb167be25921eb406354456949ef
Paul Naszalyi <pnaszaly@bouyguestelecom.fr>
2024-11-19 17:33:56 +0100

    Update icon test

--- packages/react/components/icon/test/Icon.test.tsx
+++ packages/react/components/icon/test/Icon.test.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { render } from '@testing-library/react'
 import Icon from '../Icon'
 import { IconName } from '../IconNameEnum'
-import { IconPosition, IconStatusPosition } from '../IconEnum'
+import { IconPosition } from '../IconEnum'
 import { IconProps } from '../IconProps'
 import { Alignable, TrilogyColor } from '../../../objects'
 
@@ -30,42 +30,9 @@ const defaultProps: IconProps = {
 
 describe('Icon', () => {
   it('should renders an Icon component with a name', () => {
-    const { getByTestId } = render(<Icon {...defaultProps} data-testid={'icon'}/>)
+    const { getByTestId } = render(<Icon {...defaultProps} data-testid={'icon'} />)
     const icon = getByTestId('icon')
     expect(icon).toHaveClass('icon')
     expect(icon).toBeInTheDocument()
-  })
-
-  it('should renders a status Icon component', () => {
-    const { getByTestId } = render(<Icon {...defaultProps} data-testid={'icon'} />)
-    const icon = getByTestId('icon')
-    expect(icon).toHaveClass('icon', 'is-success')
-  })
-
-  it('renders the Icon component with the correct props', () => {
-    const { getByTestId } = render(<Icon {...defaultProps} data-testid={'icon'}/>)
-    const iconElement = getByTestId('icon')
-
-    expect(iconElement).toBeInTheDocument()
-    expect(iconElement).toHaveClass('icon')
-    expect(iconElement).toHaveClass('is-medium')
-    expect(iconElement).toHaveClass('is-success')
-    expect(iconElement).not.toHaveClass('circled')
-    expect(iconElement).not.toHaveClass('loading')
-    expect(iconElement).not.toHaveClass('stacked')
-    expect(iconElement).not.toHaveClass('stretched')
-    expect(iconElement).not.toHaveClass('badge-content')
-    expect(iconElement).toHaveAttribute('data-testid', 'icon')
-    expect(iconElement).toHaveClass('test-class')
-    expect(iconElement).not.toHaveClass('test-text-class')
-  })
-
-  it('renders the CircleIcon component when circled prop is true', () => {
-    const props = { ...defaultProps, circled: true }
-    const { getByTestId } = render(<Icon {...props} data-testid={'icon'} />)
-    const iconElement = getByTestId('icon')
-
-    expect(iconElement).toBeInTheDocument()
-    expect(iconElement).toHaveClass('icon', 'is-circled')
   })
 })
