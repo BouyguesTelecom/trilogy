6166c02805ee07aa88478ca8c11fdc1f38f5823b
erwan <exavierdegetel@gmail.com>
2024-11-21 10:34:32 +0100

    fix autolayout page native

--- examples/react-template/screens/Autolayout.tsx
+++ examples/react-template/screens/Autolayout.tsx
@@ -1,124 +1,126 @@
+import { AutoComplete, AutoLayout, Section, StatusState, TrilogyColor, View } from '@trilogy-ds/react'
 import {
   Accordion,
   AccordionBody,
   AccordionHeader,
   AccordionItem,
   Alert,
-  AutoLayout,
   Box,
   BoxContent,
   BoxHeader,
   Button,
+  Card,
+  CardContent,
+  Icon,
   IconName,
-  Input,
+  IconSize,
   Text,
   TextLevels,
   Title,
   TitleLevels,
-  Icon,
-  IconSize,
-  Card,
-  CardContent,
-  Divider,
 } from '@trilogy-ds/react/components'
-import { StatusState, TrilogyColor } from '@trilogy-ds/react'
 import * as React from 'react'
 
 export const AutolayoutScreen = (): JSX.Element => {
   return (
-    <AutoLayout>
-      <Title level={TitleLevels.ONE}>Title followed by subtitle</Title>
-      <Title subtitle>subtitle</Title>
+    <View>
+      <AutoLayout>
+        <Section>
+          <Title level={TitleLevels.ONE}>Title followed by subtitle</Title>
+          <Title subtitle>subtitle</Title>
 
-      <Title level={TitleLevels.TWO}>Title followed by subtitle</Title>
-      <Title subtitle>subtitle</Title>
+          <Title level={TitleLevels.TWO}>Title followed by subtitle</Title>
+          <Title subtitle>subtitle</Title>
 
-      <Title level={TitleLevels.THREE}>Title followed by subtitle</Title>
-      <Title subtitle>subtitle</Title>
+          <Title level={TitleLevels.THREE}>Title followed by subtitle</Title>
+          <Title subtitle>subtitle</Title>
 
-      <Title level={TitleLevels.ONE}>Title followed by subtitle</Title>
-      <Title subtitle>subtitle</Title>
+          <Title level={TitleLevels.ONE}>Title followed by subtitle</Title>
+          <Title subtitle>subtitle</Title>
 
-      <Title level={TitleLevels.TWO}>Title followed by Icon</Title>
-      <Icon size={IconSize.LARGE} name={IconName.ALERT} />
-      <Title level={TitleLevels.TWO}>Title followed by Box</Title>
-      <Box>
-        <BoxHeader>Simple</BoxHeader>
-        <BoxContent>
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus tellus sed erat maximus porta. Etiam
-            non ex in dolor faucibus tempor. Sed ullamcorper, ligula sit amet dictum posuere, urna tortor vulputate
-            justo, ut luctus justo eros sed erat. Fusce finibus dolor ex.
-          </Text>
-        </BoxContent>
-      </Box>
-      <Box backgroundColor={TrilogyColor.MAIN} inverted>
-        <BoxContent>
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus tellus sed erat maximus porta. Etiam
-            non ex in dolor faucibus tempor. Sed ullamcorper, ligula sit amet dictum posuere, urna tortor vulputate
-            justo, ut luctus justo eros sed erat. Fusce finibus dolor ex.
-          </Text>
-        </BoxContent>
-      </Box>
+          <Title level={TitleLevels.TWO}>Title followed by Icon</Title>
+          <Icon size={IconSize.LARGE} name={IconName.ALERT} />
+          <Title level={TitleLevels.TWO}>Title followed by Box</Title>
+          <Box>
+            <BoxHeader>Simple</BoxHeader>
+            <BoxContent>
+              <Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus tellus sed erat maximus porta.
+                Etiam non ex in dolor faucibus tempor. Sed ullamcorper, ligula sit amet dictum posuere, urna tortor
+                vulputate justo, ut luctus justo eros sed erat. Fusce finibus dolor ex.
+              </Text>
+            </BoxContent>
+          </Box>
 
-      <Title level={TitleLevels.TWO}>Title followed by Card</Title>
+          <Box backgroundColor={TrilogyColor.MAIN} inverted>
+            <BoxContent>
+              <Text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus tellus sed erat maximus porta.
+                Etiam non ex in dolor faucibus tempor. Sed ullamcorper, ligula sit amet dictum posuere, urna tortor
+                vulputate justo, ut luctus justo eros sed erat. Fusce finibus dolor ex.
+              </Text>
+            </BoxContent>
+          </Box>
 
-      <Card active>
-        <CardContent>
-          <Text>Card content</Text>
-        </CardContent>
-      </Card>
+          <Title level={TitleLevels.TWO}>Title followed by Card</Title>
 
-      <Title level={TitleLevels.ONE}>Im a Title</Title>
+          <Card active>
+            <CardContent>
+              <Text>Card content</Text>
+            </CardContent>
+          </Card>
 
-      <Text level={TextLevels.ONE}>Lorem ipsum dolor sit amet</Text>
+          <Title level={TitleLevels.ONE}>Im a Title</Title>
 
-      <Button variant={'PRIMARY'}>Click</Button>
+          <Text level={TextLevels.ONE}>Lorem ipsum dolor sit amet</Text>
 
-      <Accordion>
-        <AccordionItem id="ONE" active={true}>
-          <AccordionHeader>
-            <Text >Hello World 1</Text>
-          </AccordionHeader>
-          <AccordionBody dataId="totooooo-test-id" testId="totooooo">
-            <Text>Lorem ipsum dolor sit amet lorem</Text>
-          </AccordionBody>
-        </AccordionItem>
-        <AccordionItem active={true} id="TWO">
-          <AccordionHeader>
-            <Text>Hello World 2</Text>
-          </AccordionHeader>
-          <AccordionBody>
-            <Text>Lorem ipsum dolor sit amet</Text>
-          </AccordionBody>
-        </AccordionItem>
-        <AccordionItem id="THREE">
-          <AccordionHeader>
-            <Text>Hello World 2</Text>
-          </AccordionHeader>
-          <AccordionBody>
-            <Text>Collpased by default</Text>
-          </AccordionBody>
-        </AccordionItem>
-        <AccordionItem disabled id="FOUR">
-          <AccordionHeader>
-            <Text>Hello World 3</Text>
-          </AccordionHeader>
-          <AccordionBody>
-            <Text>Lorem ipsum dolor sit amet</Text>
-          </AccordionBody>
-        </AccordionItem>
-      </Accordion>
+          <Button variant={'PRIMARY'}>Click</Button>
 
-      <Alert
-        display
-        status={StatusState.SUCCESS}
-        title={'Test alert'}
-        description='Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
-      />
+          <Accordion>
+            <AccordionItem id='ONE' active={true}>
+              <AccordionHeader>
+                <Text>Hello World 1</Text>
+              </AccordionHeader>
+              <AccordionBody dataId='totooooo-test-id' testId='totooooo'>
+                <Text>Lorem ipsum dolor sit amet lorem</Text>
+              </AccordionBody>
+            </AccordionItem>
+            <AccordionItem active={true} id='TWO'>
+              <AccordionHeader>
+                <Text>Hello World 2</Text>
+              </AccordionHeader>
+              <AccordionBody>
+                <Text>Lorem ipsum dolor sit amet</Text>
+              </AccordionBody>
+            </AccordionItem>
+            <AccordionItem id='THREE'>
+              <AccordionHeader>
+                <Text>Hello World 2</Text>
+              </AccordionHeader>
+              <AccordionBody>
+                <Text>Collpased by default</Text>
+              </AccordionBody>
+            </AccordionItem>
+            <AccordionItem disabled id='FOUR'>
+              <AccordionHeader>
+                <Text>Hello World 3</Text>
+              </AccordionHeader>
+              <AccordionBody>
+                <Text>Lorem ipsum dolor sit amet</Text>
+              </AccordionBody>
+            </AccordionItem>
+          </Accordion>
 
-      <Input.AutoComplete customIcon={IconName.ALERT} displayMenu={true} data={['1', '2']} />
-    </AutoLayout>
+          <Alert
+            display
+            status={StatusState.SUCCESS}
+            title={'Test alert'}
+            description='Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+          />
+
+          <AutoComplete customIcon={IconName.ALERT} displayMenu={true} data={['1', '2']} />
+        </Section>
+      </AutoLayout>
+    </View>
   )
 }
