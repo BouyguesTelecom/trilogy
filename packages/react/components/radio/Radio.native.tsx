e98c9c81bbf83abb8026c91d10cc8d381727ce7f
erwan <exavierdegetel@gmail.com>
2024-12-10 18:12:26 +0100

    fix radio

--- packages/react/components/radio/Radio.native.tsx
+++ packages/react/components/radio/Radio.native.tsx
@@ -1,10 +1,9 @@
 import { ComponentName } from '@/components/enumsComponentsName'
 import { RadioProps } from '@/components/radio/RadioProps'
 import { Text } from '@/components/text'
-import { View } from '@/components/view'
 import { getColorStyle, TrilogyColor } from '@/objects'
 import React from 'react'
-import { StyleSheet, TouchableOpacity } from 'react-native'
+import { StyleSheet, TouchableOpacity, View } from 'react-native'
 
 /**
  * radio Component
@@ -30,29 +29,26 @@ const Radio = ({
   const styles = StyleSheet.create({
     container: {
       flexDirection: 'row',
-      paddingBottom: 5,
-      justifyContent: 'flex-start',
+      alignItems: 'center',
     },
     radio: {
-      flexDirection: 'row',
       alignItems: 'center',
+      justifyContent: 'center',
       borderColor: getColorStyle(TrilogyColor.FONT),
-      borderWidth: 0.6,
-      width: 19,
-      height: 19,
+      borderWidth: 1,
+      width: 18,
+      height: 18,
       borderRadius: 10,
       marginRight: 10,
-      marginLeft: 0,
     },
     icon: {
-      position: 'absolute',
-      left: 2,
-      borderRadius: 30,
-      width: 14,
-      height: 14,
+      borderRadius: 12,
+      width: 12,
+      height: 12,
       backgroundColor: getColorStyle(TrilogyColor.MAIN),
     },
     label: {
+      paddingTop: 2,
       color:
         (disabled && getColorStyle(TrilogyColor.DISABLED)) ||
         (checked && getColorStyle(TrilogyColor.MAIN)) ||
@@ -75,14 +71,9 @@ const Radio = ({
 
   return (
     <TouchableOpacity disabled={disabled} style={styles.container} onPress={() => handleClick(value || false)}>
-      <TouchableOpacity
-        style={styles.radio}
-        disabled={disabled}
-        testID={id}
-        onPress={() => handleClick(value || false)}
-      >
+      <View style={styles.radio} testID={id}>
         {checked && <View style={styles.icon} />}
-      </TouchableOpacity>
+      </View>
       {label && typeof label.valueOf() === 'string' ? <Text style={styles.label}>{String(label)}</Text> : label}
     </TouchableOpacity>
   )
