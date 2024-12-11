297c795e76d8e4bee03f6364c383c3ddad03796a
erwan <exavierdegetel@gmail.com>
2024-12-11 11:09:40 +0100

    fis list template

--- examples/react-template/screens/List.tsx
+++ examples/react-template/screens/List.tsx
@@ -1,4 +1,3 @@
-import { getColorStyle, IconColor, Image, TrilogyColor, View } from '@trilogy-ds/react'
 import {
   IconName,
   List,
@@ -11,7 +10,6 @@ import {
   TitleLevels,
 } from '@trilogy-ds/react/components'
 import * as React from 'react'
-import { Icon } from '../../../packages/react/components'
 
 export const ListScreen = (): JSX.Element => {
   return (
@@ -20,8 +18,8 @@ export const ListScreen = (): JSX.Element => {
       <List>
         <ListItem>
           <Text level={TextLevels.ONE}>
-            <span>Hello</span>
-            <span>Ceci est la description</span>
+            <Text>Hello</Text>
+            <Text>Ceci est la description</Text>
           </Text>
         </ListItem>
         <ListItem>
@@ -36,19 +34,19 @@ export const ListScreen = (): JSX.Element => {
       <List>
         <ListItem iconName={IconName.ALERT}>
           <Text level={TextLevels.ONE}>
-            <b>Ceci est un titre</b>
+            <Text>Ceci est un titre</Text>
           </Text>
           <Text level={TextLevels.ONE}>Ceci est la description</Text>
         </ListItem>
         <ListItem iconName={IconName.ALERT}>
           <Text level={TextLevels.ONE}>
-            <b>Ceci est un titre</b>
+            <Text>Ceci est un titre</Text>
           </Text>
           <Text level={TextLevels.ONE}>Ceci est la description</Text>
         </ListItem>
         <ListItem iconName={IconName.ALERT}>
           <Text level={TextLevels.ONE}>
-            <b>Ceci est un titre</b>
+            <Text>Ceci est un titre</Text>
           </Text>
           <Text level={TextLevels.ONE}>Ceci est la description</Text>
         </ListItem>
@@ -58,51 +56,23 @@ export const ListScreen = (): JSX.Element => {
       <List divider>
         <ListItem iconName={IconName.ALERT} status={ListIconStatus.ERROR}>
           <Text level={TextLevels.ONE}>
-            <b>Ceci est un titre</b>
+            <Text>Ceci est un titre</Text>
           </Text>
           <Text level={TextLevels.ONE}>Ceci est la description</Text>
         </ListItem>
         <ListItem iconName={IconName.ALERT} status={ListIconStatus.SUCCESS}>
           <Text level={TextLevels.ONE}>
-            <b>Ceci est un titre</b>
+            <Text>Ceci est un titre</Text>
           </Text>
           <Text level={TextLevels.ONE}>Ceci est la description</Text>
         </ListItem>
         <ListItem iconName={IconName.ALERT}>
           <Text level={TextLevels.ONE}>
-            <b>Ceci est un titre</b>
+            <Text>Ceci est un titre</Text>
           </Text>
           <Text level={TextLevels.ONE}>Ceci est la description</Text>
         </ListItem>
       </List>
     </Section>
   )
-}
-
-const Dot = () => {
-  return (
-    <View
-      style={{
-        width: 8,
-        height: 8,
-        borderRadius: 8,
-        backgroundColor: getColorStyle(TrilogyColor.MAIN),
-      }}
-    />
-  )
-}
-
-const Avatar = () => {
-  return (
-    <Image
-      src='https://i.etsystatic.com/10951167/r/il/df66c4/1860902191/il_570xN.1860902191_kuoj.jpg'
-      width={40}
-      height={40}
-      circled
-    />
-  )
-}
-
-const CustomIcon = () => {
-  return <Icon size='small' name='tri-alert' color={IconColor.INFO} />
 }
