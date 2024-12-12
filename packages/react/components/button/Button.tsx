e61202fe903a129d864baa673f9b11e9246127a1
erwan <exavierdegetel@gmail.com>
2024-12-12 15:43:02 +0100

    fix keyboard navigation and init fix accessibility

--- packages/react/components/button/Button.tsx
+++ packages/react/components/button/Button.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import clsx from 'clsx'
-import { is } from '@/services/index'
-import { Loading, LoadingValues } from '@/objects/facets/Loadable'
+import { Icon } from '@/components/icon'
+import { useTrilogyContext } from '@/context'
+import { hashClass } from '@/helpers'
 import { getButtonVariantClassName } from '@/objects/facets/Color'
+import { Loading, LoadingValues } from '@/objects/facets/Loadable'
+import { is } from '@/services/index'
+import clsx from 'clsx'
+import React from 'react'
+import { ComponentName } from '../enumsComponentsName'
 import { ButtonMarkup, ButtonMarkupValues, ButtonVariant, ButtonVariantValues } from './ButtonEnum'
 import { ButtonProps } from './ButtonProps'
-import { hashClass } from '@/helpers'
-import { useTrilogyContext } from '@/context'
-import { Icon } from '@/components/icon'
 
 /**
  * Button component
@@ -32,137 +33,127 @@ import { Icon } from '@/components/icon'
  * @param type {ButtonType} button type (button|reset|submit)
  */
 
-const Button = ({
-                  markup,
-                  loading,
-                  variant,
-                  href,
-                  id,
-                  fullwidth,
-                  children,
-                  className,
-                  to,
-                  accessibilityLabel,
-                  onClick,
-                  name,
-                  routerLink,
-                  type,
-                  iconName,
-                  ...others
-                }: ButtonProps): JSX.Element => {
-  const isDisabled = others.disabled || false
-  const { styled } = useTrilogyContext()
-
-  /** Check if specified markup is valid */
-  const isCorrectMarkup = (stringMarkup: ButtonMarkup | ButtonMarkupValues) => {
-    if (
-      stringMarkup in ButtonMarkup ||
-      Object.values(ButtonMarkup).includes(stringMarkup as ButtonMarkup)
-    )
-      return true
-  }
-
-  const getClassNames = (
-    loading?: Loading | LoadingValues | boolean,
-    variant?: ButtonVariant | ButtonVariantValues,
-    fullwidth?: boolean,
-    className?: string,
-  ) => {
-    return clsx(
-      'button',
-      loading && is('loading'),
-      variant && is(getButtonVariantClassName(variant)),
-      fullwidth && is('fullwidth'),
+const Button = React.forwardRef(
+  (
+    {
+      markup,
+      loading,
+      variant,
+      href,
+      id,
+      fullwidth,
+      children,
       className,
-    )
-  }
+      to,
+      accessibilityLabel,
+      onClick,
+      name,
+      routerLink,
+      type,
+      iconName,
+      ...others
+    }: ButtonProps,
+    ref: React.Ref<HTMLButtonElement>,
+  ): JSX.Element => {
+    const isDisabled = others.disabled || false
+    const { styled } = useTrilogyContext()
 
-  const classes = hashClass(
-    styled,
-    getClassNames(loading, variant, fullwidth, className),
-  )
-  const Tag = markup && isCorrectMarkup(markup) ? markup : 'button'
+    /** Check if specified markup is valid */
+    const isCorrectMarkup = (stringMarkup: ButtonMarkup | ButtonMarkupValues) => {
+      if (stringMarkup in ButtonMarkup || Object.values(ButtonMarkup).includes(stringMarkup as ButtonMarkup))
+        return true
+    }
 
-  if (Tag === 'button' && (!href && !to)) {
+    const getClassNames = (
+      loading?: Loading | LoadingValues | boolean,
+      variant?: ButtonVariant | ButtonVariantValues,
+      fullwidth?: boolean,
+      className?: string,
+    ) => {
+      return clsx(
+        'button',
+        loading && is('loading'),
+        variant && is(getButtonVariantClassName(variant)),
+        fullwidth && is('fullwidth'),
+        className,
+      )
+    }
+
+    const classes = hashClass(styled, getClassNames(loading, variant, fullwidth, className))
+    const Tag = markup && isCorrectMarkup(markup) ? markup : 'button'
+
+    if (Tag === 'button' && !href && !to) {
+      return (
+        <button
+          ref={ref}
+          id={id}
+          aria-label={accessibilityLabel}
+          className={classes}
+          disabled={isDisabled}
+          name={name}
+          onClick={(e) => {
+            // eslint-disable-next-line no-unused-expressions
+            !isDisabled && onClick?.(e)
+            e.stopPropagation()
+          }}
+          type={type ?? 'button'}
+          {...others}
+        >
+          {iconName && <Icon className={!children ? 'is-marginless' : ''} name={iconName} />}
+          {children}
+        </button>
+      )
+    }
+
+    if (Tag === 'input') {
+      return (
+        <input
+          id={id}
+          className={classes}
+          aria-label={accessibilityLabel}
+          name={name}
+          onClick={(e) => {
+            // eslint-disable-next-line no-unused-expressions
+            !isDisabled && onClick?.(e)
+            e.stopPropagation()
+          }}
+          disabled={isDisabled}
+          type={type ?? 'submit'}
+          value={`${children}`}
+          {...others}
+        />
+      )
+    }
+
+    if (routerLink && to && !isDisabled) {
+      const RouterLink = (routerLink ? routerLink : 'a') as React.ElementType
+      return (
+        <RouterLink aria-label={accessibilityLabel} to={to} className={classes} {...others}>
+          {iconName && <Icon className={!children ? 'is-marginless' : ''} name={iconName} />}
+          {children}
+        </RouterLink>
+      )
+    }
+
     return (
-      <button
+      <a
         id={id}
         aria-label={accessibilityLabel}
         className={classes}
-        disabled={isDisabled}
-        name={name}
+        href={href}
         onClick={(e) => {
           // eslint-disable-next-line no-unused-expressions
           !isDisabled && onClick?.(e)
           e.stopPropagation()
         }}
-        type={type ?? 'button'}
         {...others}
       >
-        {iconName && (
-          <Icon className={!children ? 'is-marginless' : ''} name={iconName} />
-        )}
+        {iconName && <Icon className={!children ? 'is-marginless' : ''} name={iconName} />}
         {children}
-      </button>
+      </a>
     )
-  }
+  },
+)
 
-  if (Tag === 'input') {
-    return (
-      <input
-        id={id}
-        className={classes}
-        aria-label={accessibilityLabel}
-        name={name}
-        onClick={(e) => {
-          // eslint-disable-next-line no-unused-expressions
-          !isDisabled && onClick?.(e)
-          e.stopPropagation()
-        }}
-        disabled={isDisabled}
-        type={type ?? 'submit'}
-        value={`${children}`}
-        {...others}
-      />
-    )
-  }
-
-  if (routerLink && to && !isDisabled) {
-    const RouterLink = (routerLink ? routerLink : 'a') as React.ElementType
-    return (
-      <RouterLink
-        aria-label={accessibilityLabel}
-        to={to}
-        className={classes}
-        {...others}
-      >
-        {iconName && (
-          <Icon className={!children ? 'is-marginless' : ''} name={iconName} />
-        )}
-        {children}
-      </RouterLink>
-    )
-  }
-
-  return (
-    <a
-      id={id}
-      aria-label={accessibilityLabel}
-      className={classes}
-      href={href}
-      onClick={(e) => {
-        // eslint-disable-next-line no-unused-expressions
-        !isDisabled && onClick?.(e)
-        e.stopPropagation()
-      }}
-      {...others}
-    >
-      {iconName && (
-        <Icon className={!children ? 'is-marginless' : ''} name={iconName} />
-      )}
-      {children}
-    </a>
-  )
-}
-
+Button.displayName = ComponentName.Button
 export default Button
--- packages/react/components/modal/footer/ModalFooter.tsx
+++ packages/react/components/modal/footer/ModalFooter.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react'
-import { ModalFooterProps } from './ModalFooterProps'
+import { useTrilogyContext } from '@/context'
 import { hashClass } from '@/helpers'
 import clsx from 'clsx'
-import { useTrilogyContext } from '@/context'
+import * as React from 'react'
+import { ModalFooterProps } from './ModalFooterProps'
 
 /**
  * Modal Footer Component
@@ -14,7 +14,7 @@ import { useTrilogyContext } from '@/context'
 const ModalFooter = ({ children, className, id }: ModalFooterProps): JSX.Element => {
   const { styled } = useTrilogyContext()
   return (
-    <div id={id} className={hashClass(styled, clsx('modal-footer', className))}>
+    <div data-modal-footer='' id={id} className={hashClass(styled, clsx('modal-footer', className))}>
       {children}
     </div>
   )
