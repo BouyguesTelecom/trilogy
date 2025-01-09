bd6944eb5c6308365383979d4ee43d58be0052fb
JulienMora <julien.moratalla@gmail.com>
2025-01-09 16:47:02 +0100

    Fix Column in other component

--- packages/react/components/columns/column/Column.native.tsx
+++ packages/react/components/columns/column/Column.native.tsx
@@ -5,10 +5,7 @@ import { ComponentName } from '@/components/enumsComponentsName'
 
 /**
  * Columns Item Component - Columns Child
- * @param size {ColumnsSize} Size 1-12
- * @param mobileSize {ColumnsSize} if size is missing.
  * @param children {React.ReactNode}
- * @param verticalCentered {boolean} Vertical center Column item
  */
 const Column = ({ children, ...others }: ColumnProps): JSX.Element => {
   return <View {...others}>{children}</View>
