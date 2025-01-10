107f4608530f33d272922b7c91ba29773b42c6ea
LEGRELLE, Félix <felegrel@bouyguestelecom.fr>
2025-01-10 15:38:14 +0100

    ✨ Add Link props

--- packages/react/components/link/LinkProps.ts
+++ packages/react/components/link/LinkProps.ts
@@ -1,14 +1,12 @@
 import { IconName, IconNameValues } from '../../components/icon'
-import { Accessibility, Clickable, Dev } from '../../objects/facets'
+import { Accessibility, Clickable, Dev, Link } from '../../objects/facets'
 import { CommonProps } from '../../objects/facets/CommonProps'
 
 /**
  * Link Interface
  */
-export interface LinkProps extends Accessibility, Clickable, Dev, CommonProps {
+export interface LinkProps extends Accessibility, Clickable, Link, Dev, CommonProps {
   children?: React.ReactNode
-  to?: string
-  href?: string
   routerLink?: React.ElementType
   iconName?: IconName | IconNameValues
   inline?: boolean
