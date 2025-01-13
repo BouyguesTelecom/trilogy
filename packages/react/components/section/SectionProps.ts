1490b84c6717eae8c9fc3f3cb5b141109151c8da
JulienMora <julien.moratalla@gmail.com>
2025-01-13 11:50:33 +0100

    Fix others styles

--- packages/react/components/section/SectionProps.ts
+++ packages/react/components/section/SectionProps.ts
@@ -1,9 +1,13 @@
 import type { BackgroundProps, ChildrenWithNoText } from '../../objects'
 import { CommonProps } from '../../objects/facets/CommonProps'
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Styles = { [key: string]: any }
+
 /**
  * Section Interface
  */
 export interface SectionProps extends BackgroundProps, ChildrenWithNoText, CommonProps {
   skeleton?: boolean
+  style?: Styles
 }
