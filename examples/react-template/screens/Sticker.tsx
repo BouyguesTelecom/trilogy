71b7c491def03dc8dc69c1e959b2bd2573cf6a40
JulienMora <julien.moratalla@gmail.com>
2025-01-14 16:23:55 +0100

    Sticker with icon

--- examples/react-template/screens/Sticker.tsx
+++ examples/react-template/screens/Sticker.tsx
@@ -1,10 +1,24 @@
-import { Box, BoxContent, Section, Spacer, SpacerSize, Sticker } from '@trilogy-ds/react/components'
-import { Price, VariantState } from '@trilogy-ds/react'
+import { Box, BoxContent, Section, Spacer, SpacerSize, Sticker, IconName } from '@trilogy-ds/react/components'
+import {Price, TagVariant, VariantState} from '@trilogy-ds/react'
 
 export const StickerScreen = (): JSX.Element => {
   return (
     <Section>
       <Sticker label='Sticker' />
+
+      <Sticker
+        iconName={IconName.EYE}
+        small
+        label={'Sticker label !'}
+        variant={TagVariant.INFO}
+      />
+
+      <Sticker
+        iconName={IconName.EYE}
+        label={'Sticker label !'}
+        variant={TagVariant.INFO}
+      />
+
       <Sticker variant={VariantState.ACCENT} label='Sticker' />
       <Sticker variant={VariantState.INFO} label='Sticker' />
 
