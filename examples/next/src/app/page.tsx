9a381496f39bf35a6832278c0d7be5b280e0ce87
erwan <exavierdegetel@gmail.com>
2025-01-21 17:17:30 +0100

    fix accordion

--- examples/next/src/app/page.tsx
+++ examples/next/src/app/page.tsx
@@ -1,10 +1,7 @@
-import '@trilogy-ds/styles/dist/default/trilogy.css'
-import * as React from "react";
 export default function Home() {
   return (
     <div>
-      <main>
-      </main>
+      <main></main>
     </div>
   )
 }
--- packages/react/components/accordion/item/body/AccordionBody.tsx
+++ packages/react/components/accordion/item/body/AccordionBody.tsx
@@ -1,4 +1,5 @@
 import { AccordionBodyProps } from '@/components/accordion/item/body/AccordionBodyProps'
+import { useAccordionBody } from '@/components/accordion/item/body/hooks/useAccordionBoy'
 import { hashClass } from '@/helpers/hashClassesHelpers'
 import clsx from 'clsx'
 import React from 'react'
@@ -11,15 +12,9 @@ import React from 'react'
  * @param dataId {string} data attribute
  */
 const AccordionBody = ({ children, className, id, ...others }: AccordionBodyProps): React.JSX.Element => {
+  const { onClick } = useAccordionBody()
   return (
-    <div
-      id={id}
-      className={hashClass(clsx('accordion-body', className))}
-      onClick={(e) => {
-        e.stopPropagation()
-      }}
-      {...others}
-    >
+    <div id={id} className={hashClass(clsx('accordion-body', className))} onClick={onClick} {...others}>
       {children}
     </div>
   )
