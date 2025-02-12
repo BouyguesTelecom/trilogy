4fa8aba3474f340433f15c23db8f29870f66a6ca
erwan <exavierdegetel@gmail.com>
2025-02-12 15:24:27 +0100

    init refacto modal

---
+++ packages/react/components/modal/context/ModalContext.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import { ModalContextProps } from './ModalContextProps'
+
+export const ModalContext = React.createContext<ModalContextProps>({ scrollViewRef: null, handleOnScroll: () => null })
