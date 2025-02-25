798740a8dd973032b72d26da8a8aec23634f7f15
erwan <exavierdegetel@gmail.com>
2025-02-25 14:57:08 +0100

    ref button

--- packages/react/components/button/list/ButtonList.native.tsx
+++ packages/react/components/button/list/ButtonList.native.tsx
@@ -1,17 +1,15 @@
-import * as React from "react"
-import { View } from '@/components/view'
-import type { ButtonListProps } from "./ButtonListProps"
-import { ComponentName } from "@/components/enumsComponentsName"
+import { ComponentName } from '@/components/enumsComponentsName'
+import * as React from 'react'
+import { View } from 'react-native'
+import type { ButtonListNativeRef, ButtonListProps } from './ButtonListProps'
 
 /**
  * ButtonList Native Component
  * @param children {ReactNode} ButtonList children
  */
-const ButtonList = ({
-  children,
-}: ButtonListProps): JSX.Element => {
-  return <View {...{ children }} />
-}
+const ButtonList = React.forwardRef<ButtonListNativeRef, ButtonListProps>(({ children }, ref): JSX.Element => {
+  return <View ref={ref} {...{ children }} />
+})
 
 ButtonList.displayName = ComponentName.ButtonList
 
