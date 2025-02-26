43628dbd87a3785b127b6ed1a7f0f210784d21d9
erwan <exavierdegetel@gmail.com>
2025-02-26 14:51:12 +0100

    add ref to list

--- packages/react/components/list/item/description/ListItemDescriptionProps.ts
+++ packages/react/components/list/item/description/ListItemDescriptionProps.ts
@@ -2,8 +2,12 @@
  * ListItem Interface
  */
 import { ReactNode } from 'react'
+import { type View } from 'react-native'
 
 export interface ListItemDescriptionProps {
   children?: ReactNode
   className?: string
 }
+
+export type ListItemDescriptionRef = HTMLElement
+export type ListItemDescriptionNativeRef = View
