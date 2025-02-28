caadba77d078fd178d199e8d476a7ad023b3de03
Neal <nelahian@bt0d0000.w2k.bouyguestelecom.fr>
2025-02-28 11:19:54 +0100

    :lipstick: Remove borderBottomLeft/Right Radius Modal.native

--- packages/react/components/modal/Modal.native.tsx
+++ packages/react/components/modal/Modal.native.tsx
@@ -121,6 +121,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
-    borderRadius: 6,
+    borderTopLeftRadius: 6,
+    borderTopRightRadius: 6,
   },
 })
