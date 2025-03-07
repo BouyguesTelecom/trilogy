4c82e694ff643345e656861207e976b6ecdc89b2
erwan <exavierdegetel@gmail.com>
2025-03-07 14:29:24 +0100

    replace div to span

--- packages/react/components/popover/Popover.tsx
+++ packages/react/components/popover/Popover.tsx
@@ -1,10 +1,10 @@
-import * as React from 'react'
-import { is } from '@/services/classify'
-import { PopoverRef, PopoverWebProps } from './PopoverProps'
-import { hashClass } from '@/helpers/hashClassesHelpers'
-import clsx from 'clsx'
 import { useTrilogyContext } from '@/context/index'
+import { hashClass } from '@/helpers/hashClassesHelpers'
+import { is } from '@/services/classify'
+import clsx from 'clsx'
+import * as React from 'react'
 import { ComponentName } from '../enumsComponentsName'
+import { PopoverRef, PopoverWebProps } from './PopoverProps'
 
 /**
  * Popover Component
@@ -17,36 +17,29 @@ import { ComponentName } from '../enumsComponentsName'
  * @param testId {string} Test Id for Test Integration
  * @param arrowPosition {PopoverArrowPosition} Position of the popover arrow
  */
-const Popover = React.forwardRef<PopoverRef, PopoverWebProps>(({
-                   className,
-                   id,
-                   direction,
-                   children,
-                   active,
-                   arrowPosition,
-                   trigger,
-                   ...others
-                 }, ref): JSX.Element => {
-  const { styled } = useTrilogyContext()
+const Popover = React.forwardRef<PopoverRef, PopoverWebProps>(
+  ({ className, id, direction, children, active, arrowPosition, trigger, ...others }, ref): JSX.Element => {
+    const { styled } = useTrilogyContext()
 
-  const classes = hashClass(
-    styled,
-    clsx(
-      'popover',
-      direction && is(`popover-${direction}`),
-      active && is('popover-active'),
-      arrowPosition && is(`arrow-${arrowPosition}`),
-      className,
-    ),
-  )
+    const classes = hashClass(
+      styled,
+      clsx(
+        'popover',
+        direction && is(`popover-${direction}`),
+        active && is('popover-active'),
+        arrowPosition && is(`arrow-${arrowPosition}`),
+        className,
+      ),
+    )
 
-  return (
-    <div ref={ref} id={id} className={classes} {...others}>
-      <div className={hashClass(styled, 'popover-content')}>{children}</div>
-      {trigger && trigger}
-    </div>
-  )
-})
+    return (
+      <span ref={ref} id={id} className={classes} {...others}>
+        <span className={hashClass(styled, 'popover-content')}>{children}</span>
+        {trigger && trigger}
+      </span>
+    )
+  },
+)
 
 Popover.displayName = ComponentName.Popover
 export default Popover
