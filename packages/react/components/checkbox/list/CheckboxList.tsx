158ff1dac5786c9c5c68406d2297e0c687cbcd07
JulienMora <julien.moratalla@gmail.com>
2025-03-17 09:41:24 +0100

    Add parent for checkboxes + accessibility

--- packages/react/components/checkbox/list/CheckboxList.tsx
+++ packages/react/components/checkbox/list/CheckboxList.tsx
@@ -5,19 +5,19 @@ import { getJustifiedClassName } from '@/objects'
 import { is } from '@/services'
 import clsx from 'clsx'
 import * as React from 'react'
-import { RadioListRef, RadioListWebProps } from './RadioListProps'
+import { CheckboxListRef, CheckboxListWebProps } from './CheckboxListProps'
 
 /**
- * Radio List Component
+ * Checkbox List Component
  * @param children {ReactNode} CheckboxList children
  * - -------------------------- WEB PROPERTIES -------------------------------
  * @param className {string} Additionnal CSS Classes
- * @param align {boolean} align radios
- * @param verticalDesktop {boolean} Vertical radios
+ * @param align {boolean} align Checkboxes
+ * @param verticalDesktop {boolean} Vertical Checkboxes
  * @param horizontalMobile {boolean} Expect mobile screen
  * @param accessibilityLabelledBy {string} aria-labelledby attribute
  */
-const RadioList = React.forwardRef<RadioListRef, RadioListWebProps>(
+const CheckboxList = React.forwardRef<CheckboxListRef, CheckboxListWebProps>(
   ({ className, id, align, horizontalMobile, verticalDesktop, accessibilityLabelledBy, children, ...others }, ref): JSX.Element => {
     const { styled } = useTrilogyContext()
 
@@ -30,13 +30,13 @@ const RadioList = React.forwardRef<RadioListRef, RadioListWebProps>(
       <div
         ref={ref}
         id={id}
-        role="radiogroup"
+        role="group"
         aria-labelledby={accessibilityLabelledBy}
         aria-required={isRequired ? 'true' : undefined}
         className={hashClass(
           styled,
           clsx(
-            'radios',
+            'checkboxes',
             className,
             align && is(getJustifiedClassName(align)),
             horizontalMobile && is('horizontal-mobile'),
@@ -51,5 +51,5 @@ const RadioList = React.forwardRef<RadioListRef, RadioListWebProps>(
   },
 )
 
-RadioList.displayName = ComponentName.RadioList
-export default RadioList
+CheckboxList.displayName = ComponentName.CheckboxList
+export default CheckboxList
