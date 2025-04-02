e8b3d60f6d92685737d0ad0a65cb53cf497baa10
JulienMora <julien.moratalla@gmail.com>
2025-04-02 14:35:08 +0200

    Update

--- packages/locales/en/src/index.ts
+++ packages/locales/en/src/index.ts
@@ -2,5 +2,6 @@ import breadcrumbLocale from './breadcrumb.json'
 import inputLocale from './input.json'
 import modalLocale from './modal.json'
 import otpLocale from './otp.json'
+import testLocal from './test.json'
 
-export { breadcrumbLocale, inputLocale, modalLocale, otpLocale }
+export { breadcrumbLocale, inputLocale, modalLocale, otpLocale, testLocal }
