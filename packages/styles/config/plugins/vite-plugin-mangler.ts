2e15a95b78afc63be81de43ea03f5babf1e66ae8
Weckr Stephane <swecker@bouyguestelecom.fr>
2025-04-03 15:57:22 +0200

    One regex instead of two, the second that corrected the first

--- packages/styles/config/plugins/vite-plugin-mangler.ts
+++ packages/styles/config/plugins/vite-plugin-mangler.ts
@@ -6,10 +6,9 @@ export const cssPlugin = () => {
     transform(src: string, id: string) {
       if (/mangled\.(scss)$/.test(id)) {
         return src
-          .replace(/(\.[a-z][_\-0-9a-z]*)(?=[^\{]*{)/gi, (match) => {
+          .replace(/(\.[a-z][_\-0-9a-z]*)(?=[^\{'")]*{)/gi, (match) => {
             return `${match}_${VERSION}`
           })
-          .replace(/(url\(["']?.*?)(_[\w-]+)(.*?["']?\))/g, '$1$3')
       }
     },
   }
