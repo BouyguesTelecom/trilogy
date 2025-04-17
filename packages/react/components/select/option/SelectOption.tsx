f2c7092ef7acf4ab64d6ac5afdb44d2ee31a2030
erwan <exavierdegetel@gmail.com>
2025-04-17 18:29:24 +0200

    fix select and clean

--- packages/react/components/select/option/SelectOption.tsx
+++ packages/react/components/select/option/SelectOption.tsx
@@ -1,11 +1,12 @@
+import { ComponentName } from '@/components/enumsComponentsName'
 import { Icon } from '@/components/icon'
 import { useTrilogyContext } from '@/context'
 import { hashClass } from '@/helpers'
 import { is } from '@/services/classify'
 import clsx from 'clsx'
 import * as React from 'react'
+import { SelectContext } from '../context'
 import { SelectOptionProps, SelectOptionRef } from './SelectOptionProps'
-import { ComponentName } from '@/components/enumsComponentsName'
 
 /**
  * Select Option Component
@@ -21,23 +22,65 @@ import { ComponentName } from '@/components/enumsComponentsName'
  * @param id {string} Select option custom id
  * @param others
  */
-const SelectOption = React.forwardRef<SelectOptionRef, SelectOptionProps>(({
-  id,
-  className,
-  value,
-  disabled,
-  children,
-  onClick,
-  label,
-  iconName,
-  testId,
-  ...others
-}, ref) => {
-  const { styled } = useTrilogyContext()
-  const { checked, native, focused, ...props } = others as { checked: boolean; native: boolean; focused: boolean }
-  const selectClasses = hashClass(styled, clsx('option', focused && 'focus', disabled && is('disabled'), className))
+const SelectOption = React.forwardRef<SelectOptionRef, SelectOptionProps>(
+  ({ id, className, value = '', disabled, children, onClick, label, iconName, testId, ...others }, ref) => {
+    const { styled } = useTrilogyContext()
 
-  if (native) {
+    const { custom, selectedOptions, setSelectedOptions, multiple, setIsVisibleOptions, onChange } =
+      React.useContext(SelectContext)
+
+    const isChecked = selectedOptions.includes(value)
+    const selectClasses = hashClass(styled, clsx('option', isChecked && 'focus', disabled && is('disabled'), className))
+
+    const handleClickOption = () => {
+      setSelectedOptions((prev: string[]) => {
+        const isInclude = prev.includes(value)
+
+        const newOptionsSelected =
+          !multiple && isInclude
+            ? []
+            : !multiple && !isInclude
+            ? [value]
+            : isInclude
+            ? prev.filter((opt) => opt !== value)
+            : [...prev, value]
+
+        if (onChange) {
+          onChange({
+            selectValue: isInclude ? undefined : value,
+            selectName: isInclude ? undefined : children || label,
+            selectId: isInclude ? undefined : id,
+            name: isInclude ? undefined : children || label,
+            selectedOptions: newOptionsSelected,
+            target: undefined as unknown as EventTarget & HTMLSelectElement,
+          })
+        }
+
+        return newOptionsSelected
+      })
+
+      if (!multiple) setIsVisibleOptions(false)
+    }
+
+    if (custom || multiple) {
+      return (
+        <li
+          ref={ref as React.RefObject<HTMLLIElement>}
+          id={id}
+          className={selectClasses}
+          data-selected={isChecked}
+          role='option'
+          aria-selected={isChecked}
+          data-value={value}
+          onClick={!disabled ? handleClickOption : undefined}
+          {...others}
+        >
+          {iconName && <Icon name={iconName} />}
+          {label || children}
+        </li>
+      )
+    }
+
     return (
       <option
         ref={ref as React.RefObject<HTMLOptionElement>}
@@ -47,45 +90,13 @@ const SelectOption = React.forwardRef<SelectOptionRef, SelectOptionProps>(({
         disabled={disabled}
         aria-label={label}
         data-testid={testId}
-        onClick={onClick}
-        {...props}
+        {...others}
       >
         {children || label}
       </option>
     )
-  }
-
-  // return (
-  //   <RadioTile
-  //     checked={checked}
-  //     horizontal
-  //     className={selectClasses}
-  //     value={value}
-  //     disabled={disabled}
-  //     onChange={onClick}
-  //     icon={iconName}
-  //     description={label || children}
-  //     {...others}
-  //   />
-  // )
-
-  return (
-    <li
-      ref={ref as React.RefObject<HTMLLIElement>}
-      id={id}
-      className={selectClasses}
-      data-selected={checked}
-      role='option'
-      aria-selected={checked}
-      data-value={value}
-      onClick={!disabled && onClick ? onClick : undefined}
-      {...others}
-    >
-      {iconName && <Icon name={iconName} />}
-      {label || children}
-    </li>
-  )
-})
+  },
+)
 
 SelectOption.displayName = ComponentName.SelectOption
 export default SelectOption
