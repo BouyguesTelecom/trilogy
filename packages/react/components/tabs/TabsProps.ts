5cf4f551030d03c05c7a0201d5e6a4e55a08d602
erwan <exavierdegetel@gmail.com>
2025-04-23 16:36:32 +0200

    update design of tabs

--- packages/react/components/tabs/TabsProps.ts
+++ packages/react/components/tabs/TabsProps.ts
@@ -10,6 +10,7 @@ export interface TabsProps extends AlignableProps, Clickable, Accessibility, Dev
   activeIndex?: number
   fullwidth?: boolean
   inverted?: boolean
+  small?: boolean
 }
 
 export type TabsRef = HTMLDivElement
