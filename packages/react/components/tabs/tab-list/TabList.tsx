e5bb851b009e0b0e9e46cc71dfec225ccb7ec96b
erwan <exavierdegetel@gmail.com>
2025-04-24 16:41:17 +0200

    init arrow fnc

--- packages/react/components/tabs/tab-list/TabList.tsx
+++ packages/react/components/tabs/tab-list/TabList.tsx
@@ -1,4 +1,5 @@
 import { ComponentName } from '@/components/enumsComponentsName'
+import { Icon } from '@/components/icon'
 import Tab from '@/components/tabs/tab-list/tab/Tab'
 import { TabListProps, TabListRef } from '@/components/tabs/tab-list/TabListProps'
 import { useTrilogyContext } from '@/context'
@@ -6,7 +7,7 @@ import { hashClass } from '@/helpers/hashClassesHelpers'
 import { getAlignClassName } from '@/objects/facets/Alignable'
 import { is } from '@/services'
 import clsx from 'clsx'
-import React from 'react'
+import React, { useMemo } from 'react'
 
 /**
  * Tabs Nav Component
@@ -17,19 +18,51 @@ import React from 'react'
  * @param align
  * @param others
  */
-const TabList = React.forwardRef<TabListRef, TabListProps>(({ children, className, id, testId, align, ...others }, ref) => {
-  const { styled } = useTrilogyContext()
-  const classes = hashClass(styled, clsx('tab-list', align && is(getAlignClassName(align)), className))
+const TabList = React.forwardRef<TabListRef, TabListProps>(
+  ({ children, className, id, testId, align, ...others }, ref) => {
+    const { styled } = useTrilogyContext()
+    const innerRef = React.useRef<HTMLDivElement>(null)
 
-  return (
-    <div ref={ref} id={id} data-testid={testId} className={classes} {...others}>
-      {React.Children.map(children, (child, index) => {
-        if (!React.isValidElement(child)) return false
-        return <Tab {...child.props} index={index} />
-      })}
-    </div>
-  )
-})
+    const classes = hashClass(styled, clsx('tab-list', align && is(getAlignClassName(align)), className))
+
+    const [tabsWidth, setTabsWidth] = React.useState<number>(0)
+    const [tabListWidth, setTabListWidth] = React.useState<number>(0)
+    const [scrollLeft, setScrollLeft] = React.useState<number>(0)
+
+    const isVisibleArrowRight = useMemo(
+      () => tabListWidth - tabsWidth - scrollLeft > 0,
+      [tabListWidth, tabListWidth, scrollLeft],
+    )
+
+    const isVisibleArrowLeft = useMemo(() => scrollLeft > 0, [scrollLeft])
+
+    const handleScrollList = React.useCallback((e: React.UIEvent<HTMLDivElement, UIEvent>) => {
+      setScrollLeft((e.target as HTMLDivElement).scrollLeft)
+    }, [])
+
+    React.useImperativeHandle(ref, () => innerRef.current as HTMLDivElement)
+
+    React.useEffect(() => {
+      if (innerRef.current) {
+        setTabsWidth(innerRef.current.clientWidth)
+        setTabListWidth(innerRef.current.scrollWidth)
+      }
+    }, [innerRef])
+
+    return (
+      <>
+        <Icon name='tri-arrow-left' className={clsx('arrow-prev', !isVisibleArrowLeft && 'hidden')} />
+        <div ref={innerRef} id={id} data-testid={testId} className={classes} {...others} onScroll={handleScrollList}>
+          {React.Children.map(children, (child, index) => {
+            if (!React.isValidElement(child)) return false
+            return <Tab {...child.props} index={index} />
+          })}
+        </div>
+        <Icon name='tri-arrow-right' className={clsx('arrow-next', !isVisibleArrowRight && 'hidden')} />
+      </>
+    )
+  },
+)
 
 TabList.displayName = ComponentName.TabList
 export default TabList
