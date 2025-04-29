538d7acbc21884cddd4cf163086aeeb66265872d
erwan <exavierdegetel@gmail.com>
2025-04-29 16:14:18 +0200

    init tabs native

--- packages/react/components/tabs/tab-list/TabList.native.tsx
+++ packages/react/components/tabs/tab-list/TabList.native.tsx
@@ -1,10 +1,12 @@
 import { ComponentName } from '@/components/enumsComponentsName'
+import { Icon } from '@/components/icon'
 import { TabsContext } from '@/components/tabs/context'
 import Tab from '@/components/tabs/tab-list/tab/Tab'
 import { TabListNativeRef, TabListProps } from '@/components/tabs/tab-list/TabListProps'
 import { getColorStyle, TrilogyColor } from '@/objects'
 import React from 'react'
-import { ScrollView, StyleSheet } from 'react-native'
+import { ScrollView, StyleSheet, View } from 'react-native'
+import { TabRef } from './tab/TabProps'
 
 /**
  * Tabs Nav Component
@@ -17,22 +19,45 @@ import { ScrollView, StyleSheet } from 'react-native'
  */
 const TabList = React.forwardRef<TabListNativeRef, TabListProps>(({ children, ...others }, ref) => {
   const { inverted } = React.useContext(TabsContext)
+  const TabListRef = React.useRef<ScrollView>(null)
+  const tabRefs = React.useRef<TabRef[]>([])
+
+  const TabElms = React.useMemo(() => {
+    return React.Children.map(children, (child, index) => {
+      if (!React.isValidElement(child)) return false
+      return <Tab ref={(el) => (tabRefs.current[index] = el as TabRef)} index={index} {...child.props} />
+    })
+  }, [children, tabRefs])
 
   const styles = StyleSheet.create({
     tabList: {
       flexDirection: 'row',
-      overflow: 'visible',
       backgroundColor: getColorStyle(inverted ? TrilogyColor.MAIN : 'transparent'),
     },
   })
 
   return (
-    <ScrollView ref={ref} horizontal showsHorizontalScrollIndicator={false} style={styles.tabList} {...others}>
-      {React.Children.map(children, (child, index) => {
-        if (!React.isValidElement(child)) return false
-        return <Tab {...child.props} index={index} />
-      })}
-    </ScrollView>
+    <View
+      style={{ flexDirection: 'row', alignItems: 'center' }}
+      onLayout={(e) => {
+        console.log('container', e.nativeEvent.layout.width)
+      }}
+    >
+      <Icon name='tri-arrow-left' align='ALIGNED_CENTER' />
+      <ScrollView
+        onLayout={(e) => {
+          console.log('scroll', e.nativeEvent.layout.width)
+        }}
+        ref={TabListRef}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        style={styles.tabList}
+        {...others}
+      >
+        {TabElms}
+      </ScrollView>
+      <Icon name='tri-arrow-right' align='ALIGNED_CENTER' />
+    </View>
   )
 })
 
