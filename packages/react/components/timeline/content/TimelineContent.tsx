eeff27dc954d1b170b7a0eb82abc2e115c9a0d5b
erwan <exavierdegetel@gmail.com>
2025-06-04 14:46:18 +0200

    Add prop blank to timelineContent

--- packages/react/components/timeline/content/TimelineContent.tsx
+++ packages/react/components/timeline/content/TimelineContent.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react'
-import clsx from 'clsx'
-import { TimelineContentRef, TimelineContentWebProps } from './TimelineContentProps'
-import { Text, TextMarkup } from '@/components/text'
-import { Link } from '@/components/link'
-import { hashClass } from '@/helpers'
-import { useTrilogyContext } from '@/context'
 import { ComponentName } from '@/components/enumsComponentsName'
+import { Link } from '@/components/link'
+import { Text, TextMarkup } from '@/components/text'
+import { useTrilogyContext } from '@/context'
+import { hashClass } from '@/helpers'
+import clsx from 'clsx'
+import * as React from 'react'
+import { TimelineContentRef, TimelineContentWebProps } from './TimelineContentProps'
 
 /**
  * Timeline Content Component
@@ -16,39 +16,36 @@ import { ComponentName } from '@/components/enumsComponentsName'
  * - -------------------------- WEB PROPERTIES -------------------------------
  * @param className {string} Additional CSS Classes
  */
-const TimelineContent = React.forwardRef<TimelineContentRef, TimelineContentWebProps>(({
-  children,
-  className,
-  id,
-  heading,
-  content,
-  linkLabel,
-  linkTo,
-  ...others
-}, ref): JSX.Element => {
-  const { styled } = useTrilogyContext()
-  const classes = hashClass(styled, clsx('timeline-content', className))
+const TimelineContent = React.forwardRef<TimelineContentRef, TimelineContentWebProps>(
+  ({ children, className, id, heading, content, linkLabel, linkTo, linkBlank, ...others }, ref): JSX.Element => {
+    const { styled } = useTrilogyContext()
+    const classes = hashClass(styled, clsx('timeline-content', className))
 
-  if (children) {
+    if (children) {
+      return (
+        <div ref={ref} id={id} className={classes} {...others}>
+          {children}
+        </div>
+      )
+    }
+
     return (
       <div ref={ref} id={id} className={classes} {...others}>
-        {children}
+        {heading && <Text markup={TextMarkup.P}>{heading}</Text>}
+        {content && (
+          <Text className='main-content' markup={TextMarkup.P}>
+            {content}
+          </Text>
+        )}
+        {linkTo && linkLabel && (
+          <Link href={linkTo} blank={linkBlank}>
+            {linkLabel}
+          </Link>
+        )}
       </div>
     )
-  }
-
-  return (
-    <div ref={ref} id={id} className={classes} {...others}>
-      {heading && <Text markup={TextMarkup.P}>{heading}</Text>}
-      {content && (
-        <Text className='main-content' markup={TextMarkup.P}>
-          {content}
-        </Text>
-      )}
-      {linkTo && linkLabel && <Link href={linkTo}>{linkLabel}</Link>}
-    </div>
-  )
-})
+  },
+)
 
 TimelineContent.displayName = ComponentName.TimelineContent
 export default TimelineContent
