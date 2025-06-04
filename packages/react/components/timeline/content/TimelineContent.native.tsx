8c55c6553e5d983c9ef08bc5aa99c5db27d943c2
erwan <exavierdegetel@gmail.com>
2025-06-04 14:50:30 +0200

    update jsdoc

--- packages/react/components/timeline/content/TimelineContent.native.tsx
+++ packages/react/components/timeline/content/TimelineContent.native.tsx
@@ -12,59 +12,60 @@ import { StyleSheet, TouchableOpacity, View } from 'react-native'
  * Timeline Content Component
  * @param children {ReactNode} Use it if you want custom children for content
  * @param content {string} Text content
- * @param contentLink {string} Text for content link
  * @param heading {string} Text heading
- * @param link {string} Url link
- * @param Onclick {string} Provide event onCLick
+ * @param linkLabel {string} Text for content link
+ * @param linkTo {string} href of link
  */
-const TimelineContent = React.forwardRef<TimelineContentNativeRef, TimelineContentProps>(({ content, heading, linkLabel, linkTo, children }, ref): JSX.Element => {
-  const timelineContextValues = useContext(TimelineItemContext)
+const TimelineContent = React.forwardRef<TimelineContentNativeRef, TimelineContentProps>(
+  ({ content, heading, linkLabel, linkTo, children }, ref): JSX.Element => {
+    const timelineContextValues = useContext(TimelineItemContext)
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 6,
-      marginBottom: 8,
-      marginLeft: 4,
-    },
-    heading: {
-      marginBottom: 4,
-      marginTop: 8,
-    },
-    content: {
-      fontWeight: timelineContextValues.active ? '600' : '400',
-      marginBottom: 4,
-    },
-    link: {
-      marginTop: 6,
-    },
-  })
+    const styles = StyleSheet.create({
+      container: {
+        flex: 6,
+        marginBottom: 8,
+        marginLeft: 4,
+      },
+      heading: {
+        marginBottom: 4,
+        marginTop: 8,
+      },
+      content: {
+        fontWeight: timelineContextValues.active ? '600' : '400',
+        marginBottom: 4,
+      },
+      link: {
+        marginTop: 6,
+      },
+    })
 
-  return children ? (
-    <View ref={ref} style={styles.container}>
-      <AutoLayout>{children}</AutoLayout>
-    </View>
-  ) : (
-    <View ref={ref} style={styles.container}>
-      {!!heading && (
-        <Text level={TextLevels.THREE} style={styles.heading} typo={TypographyColor.TEXT_MAIN}>
-          {heading}
-        </Text>
-      )}
-      {!!content && (
-        <Text level={TextLevels.TWO} style={styles.content}>
-          {content}
-        </Text>
-      )}
-      {linkLabel && linkTo && (
-        <TouchableOpacity testID='click-id'>
-          <View style={styles.link}>
-            <Link to={linkTo}>{linkLabel}</Link>
-          </View>
-        </TouchableOpacity>
-      )}
-    </View>
-  )
-})
+    return children ? (
+      <View ref={ref} style={styles.container}>
+        <AutoLayout>{children}</AutoLayout>
+      </View>
+    ) : (
+      <View ref={ref} style={styles.container}>
+        {!!heading && (
+          <Text level={TextLevels.THREE} style={styles.heading} typo={TypographyColor.TEXT_MAIN}>
+            {heading}
+          </Text>
+        )}
+        {!!content && (
+          <Text level={TextLevels.TWO} style={styles.content}>
+            {content}
+          </Text>
+        )}
+        {linkLabel && linkTo && (
+          <TouchableOpacity testID='click-id'>
+            <View style={styles.link}>
+              <Link to={linkTo}>{linkLabel}</Link>
+            </View>
+          </TouchableOpacity>
+        )}
+      </View>
+    )
+  },
+)
 
 TimelineContent.displayName = ComponentName.TimelineContent
 
