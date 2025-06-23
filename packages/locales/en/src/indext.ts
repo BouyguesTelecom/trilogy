0a1e42a101ab01a14243dbff470efac3aff91164
erwan <exavierdegetel@gmail.com>
2025-06-23 18:25:59 +0200

    fix translation package for esm compat

---
+++ packages/locales/en/src/indext.ts
@@ -0,0 +1,6 @@
+import breadcrumbLocale from './breadcrumb'
+import inputLocale from './input'
+import modalLocale from './modal'
+import otpLocale from './otp'
+
+export { breadcrumbLocale, inputLocale, modalLocale, otpLocale }
