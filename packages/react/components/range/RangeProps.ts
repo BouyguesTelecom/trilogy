fe04143a99b4c5320a3a1f79357bb29e7c71e8e3
erwan <exavierdegetel@gmail.com>
2025-06-30 11:09:08 +0200

    update className

--- packages/react/components/range/RangeProps.ts
+++ packages/react/components/range/RangeProps.ts
@@ -5,7 +5,10 @@ import { View } from 'react-native'
 import { Accessibility, Dev } from '../../objects'
 import { CommonProps } from '../../objects/facets/CommonProps'
 
-export type InputChangeEventHandlerNative = (event: { inputValue: number[]; inputName: string }) => void
+export type InputChangeEventHandlerNative = (event: {
+  inputValue: number[] | number
+  inputName: string | undefined
+}) => void
 export type InputChangeEventHandlerWeb = (event: { inputValue: number; inputName: string | undefined }) => void
 
 export interface RangeProps extends Accessibility, Dev, CommonProps {
