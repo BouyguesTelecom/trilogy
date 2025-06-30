5e0b0eb7ebc340ac9b83da4ea5ebf27868cac2ce
erwan <exavierdegetel@gmail.com>
2025-06-30 11:53:22 +0200

    update onchange

--- examples/react-template/screens/Range.tsx
+++ examples/react-template/screens/Range.tsx
@@ -21,13 +21,10 @@ export const RangeScreen = (): JSX.Element => {
         min={0}
         max={100}
         unit={'%'}
-        valueMin={0}
-        valueMax={100}
         label='Ceci est un label'
         id={'test'}
         name='name-range'
-        onChangeMin={(e) => console.log(e)}
-        onChangeMax={(e) => console.log(e)}
+        onChange={(e) => console.log(e)}
         gap={2}
       />
     </Section>
