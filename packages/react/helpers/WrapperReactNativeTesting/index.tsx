c28992fe3683c72e37bed50f72ab3c14003daf2c
erwan <exavierdegetel@gmail.com>
2025-09-25 15:59:13 +0200

    structuration of eventd helpers and services folder for exports

--- packages/react/helpers/WrapperReactNativeTesting/index.tsx
+++ packages/react/helpers/WrapperReactNativeTesting/index.tsx
@@ -1,7 +1,7 @@
 import { SVGicons } from '@trilogy-ds/assets/lib/iconsPath'
 import * as React from 'react'
 
-import { TrilogyThemeProvider, defaultTheme } from '../context/providerTheme'
+import { TrilogyThemeProvider, defaultTheme } from '../../context/providerTheme'
 
 export const WrapperReactNativeTesting = ({ children }: { children: React.ReactNode }) => (
   <TrilogyThemeProvider theme={{ ...defaultTheme, icons: SVGicons }}>{children}</TrilogyThemeProvider>
