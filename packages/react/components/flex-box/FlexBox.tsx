d1d3ab9c63c9e396e17cb3be21d7dfa76eeac6ee
LEGRELLE, Félix <felegrel@bouyguestelecom.fr>
2025-09-29 14:32:54 +0200

    Add Flexbox reponsive align & justify

--- packages/react/components/flex-box/FlexBox.tsx
+++ packages/react/components/flex-box/FlexBox.tsx
@@ -8,6 +8,32 @@ import clsx from 'clsx'
 import React from 'react'
 import { FlexBoxProps, FlexBoxRef } from './FlexBoxProps'
 
+const getResponsiveClasses = (
+  value:
+    | string
+    | number
+    | { mobile?: string | number; tablet?: string | number; desktop?: string | number }
+    | undefined,
+  getClassName: (val: string | number) => string,
+) => {
+  if (!value) return []
+
+  if (typeof value === 'string' || typeof value === 'number') {
+    return getClassName(value)
+  }
+
+  if (typeof value === 'object') {
+    const responsiveValue = value
+    return [
+      responsiveValue.desktop && `${getClassName(responsiveValue.desktop)}-desktop`,
+      responsiveValue.mobile && `${getClassName(responsiveValue.mobile)}-mobile`,
+      responsiveValue.tablet && `${getClassName(responsiveValue.tablet)}-tablet`,
+    ].filter(Boolean)
+  }
+
+  return []
+}
+
 /**
  * @beta
  * FlexBox component
@@ -16,31 +42,24 @@ import { FlexBoxProps, FlexBoxRef } from './FlexBoxProps'
  * @param id {string} Id attribute
  * @param gap {number | { mobile?: number; tablet?: number; desktop?: number }} Gap between children
  * @param direction { 'row' | 'column' | 'row-reverse' | 'column-reverse' | { mobile?: 'row' | 'column' | 'row-reverse' | 'column-reverse'; tablet?: 'row' | 'column' | 'row-reverse' | 'column-reverse'; desktop?: 'row' | 'column' | 'row-reverse' | 'column-reverse' } } Flex direction
- * @param align { 'start' | 'end' | 'center' | 'stretch' | 'baseline' } Align items
- * @param justify { 'start' | 'end' | 'center' | 'between' | 'around' | 'evenly' } Justify content
+ * @param align { 'start' | 'end' | 'center' | 'stretch' | 'baseline' | { mobile?: 'start' | 'end' | 'center' | 'stretch' | 'baseline'; tablet?: 'start' | 'end' | 'center' | 'stretch' | 'baseline'; desktop?: 'start' | 'end' | 'center' | 'stretch' | 'baseline' } } Align items
+ * @param justify { 'start' | 'end' | 'center' | 'between' | 'around' | 'evenly' | { mobile?: 'start' | 'end' | 'center' | 'between' | 'around' | 'evenly'; tablet?: 'start' | 'end' | 'center' | 'between' | 'around' | 'evenly'; desktop?: 'start' | 'end' | 'center' | 'between' | 'around' | 'evenly' } } Justify content
  * @param scrollable {boolean} scrollable mode (overflow-x: auto)
  * @param fullheight {boolean} Full height (height: 100%)
  */
 const FlexBox = React.forwardRef<FlexBoxRef, FlexBoxProps>(
   ({ className, id, gap, direction, align, justify, scrollable, fullheight, ...others }, ref) => {
     const { styled } = useTrilogyContext()
-    const isNumber = typeof gap === 'number'
-    const isValueDirection = typeof direction === 'string'
+
     const classes = hashClass(
       styled,
       clsx(
         'flex-box',
-        isValueDirection && direction && is(`direction-${direction}`),
-        !isValueDirection && direction?.desktop && is(`direction-${direction.desktop}-desktop`),
-        !isValueDirection && direction?.mobile && is(`direction-${direction.mobile}-mobile`),
-        !isValueDirection && direction?.tablet && is(`direction-${direction.tablet}-tablet`),
-        align && is(getAlignClassName(align)),
-        justify && is(getJustifyClassName(justify)),
+        ...getResponsiveClasses(direction, (val) => is(`direction-${val}`)),
+        ...getResponsiveClasses(align, (val) => is(getAlignClassName(val as string))),
+        ...getResponsiveClasses(justify, (val) => is(getJustifyClassName(val as string))),
+        ...getResponsiveClasses(gap, (val) => has(`gap-${val}`)),
         scrollable && is('scrollable'),
-        !isNumber && gap?.mobile && has(`gap-${gap.mobile}-mobile`),
-        !isNumber && gap?.tablet && has(`gap-${gap.tablet}-tablet`),
-        !isNumber && gap?.desktop && has(`gap-${gap.desktop}-desktop`),
-        isNumber && has(`gap-${gap}`),
         fullheight && is('fullheight'),
         className,
       ),
