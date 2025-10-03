4452771b6f98c7663ef6e10c03d57ff361eaf4bc
Julien Moratalla <julien.moratalla@gmail.com>
2025-10-03 14:28:02 +0200

    Revert "Add background color transparent for interface"

--- packages/react/objects/atoms/Background.ts
+++ packages/react/objects/atoms/Background.ts
@@ -5,7 +5,7 @@ import { Invertable } from "@/objects/facets"
  * Background props
  */
 export interface BackgroundProps extends Invertable {
-  backgroundColor?: TrilogyColor | TrilogyColorValues | 'transparent'
+  backgroundColor?: TrilogyColor | TrilogyColorValues
   backgroundSrc?: string
 }
 
