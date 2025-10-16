dd820ceea9004ccc8766223f186cf405ce475d70
erwan <exavierdegetel@gmail.com>
2025-10-16 15:10:09 +0200

    fix tag color native

--- packages/react/components/tag/Tag.native.tsx
+++ packages/react/components/tag/Tag.native.tsx
@@ -1,5 +1,5 @@
 import { ComponentName } from '@/components/enumsComponentsName'
-import { Icon, IconSize } from '@/components/icon'
+import { Icon, IconColor, IconSize } from '@/components/icon'
 import { getColorStyle, TrilogyColor } from '@/objects/facets/Color'
 import { getStatusStyle } from '@/objects/facets/Status'
 import React from 'react'
@@ -15,57 +15,60 @@ import { TagNativeRef, TagProps } from './TagProps'
  * @param iconName {IconName} display icon
  * @param testId {string} Test Id for Test Integration
  **/
-const Tag = React.forwardRef<TagNativeRef, TagProps>(({ label, variant, inverted, iconName, small, testId, ...others }, ref): JSX.Element => {
-  const textColor = inverted ? getColorStyle(variant as TrilogyColor) : getColorStyle(TrilogyColor.MAIN)
+const Tag = React.forwardRef<TagNativeRef, TagProps>(
+  ({ label, variant, inverted, iconName, small, testId, ...others }, ref): JSX.Element => {
+    const textColor = inverted ? getColorStyle(variant as TrilogyColor) : getColorStyle(TrilogyColor.MAIN)
 
-  const backgroundColor = variant && getStatusStyle(variant).backgroundColor
+    const backgroundColor = variant && getStatusStyle(variant).backgroundColor
 
-  const styles = StyleSheet.create({
-    tag: {
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-      alignSelf: 'center',
-      borderRadius: 15,
-      paddingHorizontal: 8,
-      paddingVertical: 4,
-      minHeight: small ? 24 : 32,
-      backgroundColor:
-        (inverted && getColorStyle(TrilogyColor.BACKGROUND)) ||
-        (variant && (backgroundColor as TrilogyColor)) ||
-        getColorStyle(TrilogyColor.NEUTRAL_FADE),
-    },
-    text: {
-      alignSelf: 'center',
-      alignItems: 'center',
-      fontWeight: '500',
-      justifyContent: 'center',
-      color: textColor,
-      fontSize: small ? 12 : 16,
-    },
-    icon: {
-      marginRight: small ? 3 : 6,
-    },
-    button: {
-      flexDirection: 'row',
-    },
-  })
+    const styles = StyleSheet.create({
+      tag: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+        borderRadius: 15,
+        paddingHorizontal: 8,
+        paddingVertical: 4,
+        minHeight: small ? 24 : 32,
+        backgroundColor:
+          (inverted && getColorStyle(TrilogyColor.BACKGROUND)) ||
+          (variant && (backgroundColor as TrilogyColor)) ||
+          getColorStyle(TrilogyColor.NEUTRAL_FADE),
+      },
+      text: {
+        alignSelf: 'center',
+        alignItems: 'center',
+        fontWeight: '500',
+        justifyContent: 'center',
+        color: textColor,
+        fontSize: small ? 12 : 16,
+      },
+      icon: {
+        marginRight: small ? 3 : 6,
+      },
+      button: {
+        flexDirection: 'row',
+      },
+    })
 
-  return (
-    <View ref={ref} style={styles.tag} {...others}>
-      {iconName && (
-        <View style={styles.icon}>
-          <Icon
-            size={small ? IconSize.SMALLER : IconSize.SMALL}
-            name={iconName}
-            testId={`${testId}-icon`}
-          />
-        </View>
-      )}
-      <Text style={styles.text}>{label}</Text>
-    </View>
-  )
-})
+    return (
+      <View ref={ref} style={styles.tag} {...others}>
+        {iconName && (
+          <View style={styles.icon}>
+            <Icon
+              color={IconColor[variant || IconColor.BACKGROUND]}
+              size={small ? IconSize.SMALLER : IconSize.SMALL}
+              name={iconName}
+              testId={`${testId}-icon`}
+            />
+          </View>
+        )}
+        <Text style={styles.text}>{label}</Text>
+      </View>
+    )
+  },
+)
 
 Tag.displayName = ComponentName.Tag
 
