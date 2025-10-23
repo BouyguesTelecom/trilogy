538c624ecef28d6a6c6e45c46d4c8bbb987e941a
JulienMora <julien.moratalla@gmail.com>
2025-10-23 11:19:50 +0200

    Update spacing

--- examples/react-template/screens/Radio.tsx
+++ examples/react-template/screens/Radio.tsx
@@ -13,7 +13,7 @@ export const RadioScreen = (): JSX.Element => {
       <Container>
         <Title level={4}>Accessibility example</Title>
         <Text id='xx'>How would you like to be contacted ? *</Text>
-        <RadioList groupLabel='Contact Method !' accessibilityLabelledBy='xx'>
+        <RadioList groupLabel='Contact Method' accessibilityLabelledBy='xx'>
           <Radio name='Email' label='Email' value='Email' checked id='checkbox1' required />
           <Radio name='Phone' label='PhoneTéléphone' value='Phone' id='checkbox2' />
           <Radio name='letter' label='letter' value='letter' disabled id='checkbox3' />
