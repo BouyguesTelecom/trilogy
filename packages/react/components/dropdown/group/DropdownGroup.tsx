eeeff02258a2f71084945636ccbc923baa535783
JulienMora <julien.moratalla@gmail.com>
2025-12-15 10:44:25 +0100

    Update styles

--- packages/react/components/dropdown/group/DropdownGroup.tsx
+++ packages/react/components/dropdown/group/DropdownGroup.tsx
@@ -20,7 +20,6 @@ const DropdownGroup = React.forwardRef<DropdownGroupRef, DropdownGroupProps>(
       className,
       id,
       children,
-      title,
       hideSeparator,
       testId,
       ...others
