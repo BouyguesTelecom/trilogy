28c122a0c891f1c39e6139eee05b137c4e5366db
JulienMora <julien.moratalla@gmail.com>
2026-01-08 11:56:00 +0100

    update dropdown trigger

--- packages/react/components/dropdown/trigger/DropdownTrigger.tsx
+++ packages/react/components/dropdown/trigger/DropdownTrigger.tsx
@@ -1,22 +1,48 @@
 import * as React from 'react'
 import { ComponentName } from '@/components/enumsComponentsName'
+import { useTrilogyContext } from '@/context'
+import { hashClass } from '@/helpers/hashClassesHelpers'
+import clsx from 'clsx'
 import { DropdownTriggerProps, DropdownTriggerRef } from './DropdownTriggerProps'
 
 /**
  * DropdownTrigger Component
- * @param children {React.ReactNode} Children
+ * Wrapper component that makes its children clickable to trigger dropdown toggle
+ * @param children {React.ReactNode} Children - The trigger element (Button, etc.)
+ * @param onClick {Function} Click handler for toggling dropdown
+ * @param className {string} Additional CSS classes
+ * @param testId {string} Test ID
  */
 const DropdownTrigger = React.forwardRef<DropdownTriggerRef, DropdownTriggerProps>(
   (
     {
       children,
+      onClick,
+      className,
+      testId,
       ...others
     },
     ref,
   ): JSX.Element => {
+    const { styled } = useTrilogyContext()
+
+    const classes = hashClass(
+      styled,
+      clsx(
+        className,
+      ),
+    )
 
     return (
-      <div {...others} ref={ref}></div>
+      <div
+        ref={ref}
+        className={classes}
+        onClick={onClick}
+        data-testid={testId}
+        {...others}
+      >
+        {children}
+      </div>
     )
   },
 )
--- packages/react/components/dropdown/trigger/DropdownTriggerProps.ts
+++ packages/react/components/dropdown/trigger/DropdownTriggerProps.ts
@@ -1,11 +1,14 @@
-import { View } from 'react-native'
+import { TouchableOpacity, View } from 'react-native'
+import { Clickable } from '../../../objects/facets/Clickable'
+import { CommonProps } from '../../../objects/facets/CommonProps'
+import { Dev } from '../../../objects/facets/Dev'
 
 /**
  * DropdownTrigger Interface
  */
-export interface DropdownTriggerProps {
+export interface DropdownTriggerProps extends Clickable, CommonProps, Dev {
   children?: React.ReactNode
 }
 
-export type DropdownTriggerRef = HTMLDivElement | HTMLAnchorElement | HTMLButtonElement
-export type DropdownTriggerNativeRef = View
+export type DropdownTriggerRef = HTMLDivElement
+export type DropdownTriggerNativeRef = TouchableOpacity
