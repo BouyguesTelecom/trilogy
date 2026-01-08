4734e52749136f88bd0722dac66757e36f50c773
erwan <exavierdegetel@gmail.com>
2026-01-08 11:54:41 +0100

    update strucutre of folders in promptAI

--- packages/react/components/promptAi/index.ts
+++ packages/react/components/promptAi/index.ts
@@ -2,11 +2,11 @@ import PromptAi from './PromptAi'
 import { PromptAiFiles } from './files'
 import PromptAiTextarea from './textarea/PromptAiTextarea'
 import PromptAiToolbar from './toolbar/PromptAiToolbar'
+import { PromptAiMicrophone } from './toolbar/microphone'
 import PromptAiSubmit from './toolbar/submit/PromptAiSubmit'
 import PromptAiTools from './toolbar/tools/PromptAiTools'
 import { PromptAiButton } from './toolbar/tools/button'
 import PromptAiInputFile from './toolbar/tools/inputFile/PromptAiInputFile'
-import { PromptAiMicrophone } from './toolbar/tools/microphone'
 import { PromptAiSelect } from './toolbar/tools/select'
 
 export {
