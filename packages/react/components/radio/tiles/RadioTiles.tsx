e0fad312d97695586ab3322151d121f414c7128b
exavier <exavier@bouyguestelecom.fr>
2026-02-17 15:50:43 +0100

    init numberCols prop on radio tiles

--- packages/react/components/radio/tiles/RadioTiles.tsx
+++ packages/react/components/radio/tiles/RadioTiles.tsx
@@ -2,9 +2,9 @@ import { ComponentName } from '@/components/enumsComponentsName'
 import { RadioTilesProps, RadioTilesRef } from '@/components/radio/tiles/RadioTilesProps'
 import { useTrilogyContext } from '@/context'
 import { hashClass } from '@/helpers'
+import { isRequiredChild } from '@/helpers/require'
 import { getAlignClassName } from '@/objects'
 import { is } from '@/services'
-import { isRequiredChild } from '@/helpers/require'
 import clsx from 'clsx'
 import * as React from 'react'
 
@@ -14,51 +14,77 @@ import * as React from 'react'
  * @param children {ReactNode}
  * @param align { Alignable | AlignableValues} align content
  * @param verticalAlign { Alignable | AlignableValues} align vertical content
+ * @param numberCols {FlexSize | FlexItemSize} number of columns for grid layout
  * - -------------------------- WEB PROPERTIES -------------------------------
  * @param className {string} Additional CSS Classes
  * @param accessibilityLabelledBy {string} aria-labelledby attribute
  */
-const RadioTiles = React.forwardRef<RadioTilesRef, RadioTilesProps>(({ id, className, children, align, verticalAlign, accessibilityLabelledBy, ...others }, ref): JSX.Element => {
-  const { styled } = useTrilogyContext()
+const RadioTiles = React.forwardRef<RadioTilesRef, RadioTilesProps>(
+  (
+    { id, className, children, align, verticalAlign, numberCols, accessibilityLabelledBy, ...others },
+    ref,
+  ): JSX.Element => {
+    const { styled } = useTrilogyContext()
+    let alignClass = null
+    let verticalAlignClass = null
 
-  let alignClass = null
+    if (align) {
+      alignClass =
+        (getAlignClassName(align) === 'aligned-start' && is('justified-start')) ||
+        (getAlignClassName(align) === 'aligned-center' && is('justified-center')) ||
+        (getAlignClassName(align) === 'aligned-end' && is('justified-end')) ||
+        null
+    }
 
-  if (align) {
-    alignClass =
-      (getAlignClassName(align) === 'aligned-start' && is('justified-start')) ||
-      (getAlignClassName(align) === 'aligned-center' && is('justified-center')) ||
-      (getAlignClassName(align) === 'aligned-end' && is('justified-end')) ||
-      null
-  }
+    if (verticalAlign) {
+      verticalAlignClass =
+        (getAlignClassName(verticalAlign) === 'aligned-start' && is('aligned-start')) ||
+        (getAlignClassName(verticalAlign) === 'aligned-center' && is('aligned-center')) ||
+        (getAlignClassName(verticalAlign) === 'aligned-end' && is('aligned-end')) ||
+        (getAlignClassName(verticalAlign) === 'aligned-stretch' && is('aligned-stretch')) ||
+        null
+    }
 
-  let verticalAlignClass = null
+    const getGridColumnClasses = () => {
+      if (!numberCols) return null
+      if (typeof numberCols === 'number') return `is-grid-cols-${numberCols}`
+      const responsiveClasses = []
+      if (numberCols.mobile) responsiveClasses.push(`is-grid-cols-mobile-${numberCols.mobile}`)
+      if (numberCols.tablet) responsiveClasses.push(`is-grid-cols-tablet-${numberCols.tablet}`)
+      if (numberCols.touch) responsiveClasses.push(`is-grid-cols-touch-${numberCols.touch}`)
+      if (numberCols.desktop) responsiveClasses.push(`is-grid-cols-desktop-${numberCols.desktop}`)
+      if (numberCols.widescreen) responsiveClasses.push(`is-grid-cols-widescreen-${numberCols.widescreen}`)
+      if (numberCols.fullhd) responsiveClasses.push(`is-grid-cols-fullhd-${numberCols.fullhd}`)
+      return responsiveClasses.join(' ')
+    }
 
-  if (verticalAlign) {
-    verticalAlignClass =
-      (getAlignClassName(verticalAlign) === 'aligned-start' && is('aligned-start')) ||
-      (getAlignClassName(verticalAlign) === 'aligned-center' && is('aligned-center')) ||
-      (getAlignClassName(verticalAlign) === 'aligned-end' && is('aligned-end')) ||
-      (getAlignClassName(verticalAlign) === 'aligned-stretch' && is('aligned-stretch')) ||
-      null
-  }
+    const gridColumnClasses = getGridColumnClasses()
 
-  return (
-    <div
-      ref={ref}
-      id={id}
-      role={"radiogroup"}
-      aria-labelledby={accessibilityLabelledBy}
-      aria-required={isRequiredChild(children) ? 'true' : undefined}
-      className={hashClass(
-        styled,
-        clsx('radio-tiles', className, align && alignClass, verticalAlign && verticalAlignClass),
-      )}
-      {...others}
-    >
-      {children}
-    </div>
-  )
-})
+    return (
+      <div
+        ref={ref}
+        id={id}
+        role={'radiogroup'}
+        aria-labelledby={accessibilityLabelledBy}
+        aria-required={isRequiredChild(children) ? 'true' : undefined}
+        className={hashClass(
+          styled,
+          clsx(
+            'radio-tiles',
+            className,
+            align && alignClass,
+            verticalAlign && verticalAlignClass,
+            gridColumnClasses && 'is-grid',
+            gridColumnClasses,
+          ),
+        )}
+        {...others}
+      >
+        {children}
+      </div>
+    )
+  },
+)
 
 RadioTiles.displayName = ComponentName.RadioTiles
 export default RadioTiles
--- packages/react/components/radio/tiles/RadioTilesProps.ts
+++ packages/react/components/radio/tiles/RadioTilesProps.ts
@@ -1,13 +1,14 @@
 import { View } from 'react-native'
 import { AlignableProps } from '../../../objects'
 import { CommonProps } from '../../../objects/facets/CommonProps'
+import { FlexItemSize, FlexSize } from '../../flex-box/flex-item/FlexItemProps'
 
-/**
- * Columns Item Interface
- */
+type NumberColors = FlexSize | FlexItemSize
+
 export interface RadioTilesProps extends AlignableProps, CommonProps {
-  children: React.ReactNode;
-  accessibilityLabelledBy?: string;
+  children: React.ReactNode
+  accessibilityLabelledBy?: string
+  numberCols?: NumberColors
 }
 
 export type RadioTilesRef = HTMLDivElement
