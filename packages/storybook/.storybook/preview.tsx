31ab89deaf2ab0c5559b5de698aa987cc373acd4
exavier <exavier@bouyguestelecom.fr>
2026-05-18 17:22:51 +0200

    update theme

--- packages/storybook/.storybook/preview.tsx
+++ packages/storybook/.storybook/preview.tsx
@@ -1,6 +1,17 @@
 import type { Decorator, Preview } from '@storybook/react'
+import { create } from '@storybook/theming/create'
 import { TrilogyProvider } from '@trilogy-ds/react/context/provider'
 import '@trilogy-ds/styles/dist/default/trilogy-mangled.css'
+
+const trilogyTheme = create({
+  base: 'light',
+  brandTitle: 'Trilogy',
+  brandTarget: '_self',
+  colorPrimary: '#3d5d7e',
+  colorSecondary: '#3d5d7e',
+  fontBase: '"Poppins", sans-serif',
+  fontCode: 'monospace',
+})
 
 const withTrilogyProvider: Decorator = (Story) => (
   <TrilogyProvider mangled>
@@ -22,6 +33,7 @@ const preview: Preview = {
     },
     docs: {
       toc: true,
+      theme: trilogyTheme,
     },
   },
 }
