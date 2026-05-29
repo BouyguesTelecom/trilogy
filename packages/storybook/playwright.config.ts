7ce500589183fd5388d1671629a77ac4d1018353
exavier <exavier@bouyguestelecom.fr>
2026-05-29 10:11:53 +0200

    move visual spec into test component folder

--- packages/storybook/playwright.config.ts
+++ packages/storybook/playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: '../react/components',
   expect: {
     toHaveScreenshot: {
       animations: 'disabled',
