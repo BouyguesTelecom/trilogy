8960231f270760cb25211f97abbd03c4e5c62aa9
exavier <exavier@bouyguestelecom.fr>
2026-07-31 09:49:05 +0200

    fix type

--- packages/react/context/interfaces.ts
+++ packages/react/context/interfaces.ts
@@ -3,6 +3,6 @@ import type { ImageSourcePropType } from 'react-native'
 export interface ITrilogyTheme {
   icons?: { [key: string]: ImageSourcePropType }
   colors: { [key: string]: [string, string, string] }
-  fontFamily?: { regular: string; medium: string; bold: string }
+  fontFamily?: { regular: string; medium: string; bold: string; speak?: string }
   radius?: { smaller: number; small: number; medium: number; large: number; full: number }
 }
