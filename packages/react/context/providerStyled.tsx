f5d591a027064310dff52d2eff56229f1fbb160f
exavier <exavier@bouyguestelecom.fr>
2026-08-24 10:22:01 +0200

    delete auto inject style from provider

--- packages/react/context/providerStyled.tsx
+++ packages/react/context/providerStyled.tsx
@@ -6,53 +6,25 @@ import { TrilogyContext } from './index'
 
 interface TrilogyProviderStyledProps {
   children: React.ReactNode
-  theme?: 'default' | 'mangled' | 'none'
+  mangled?: boolean
   hash?: string
 }
 
 /**
  * Trilogy Provider With Style
  * @param children App
- * @param theme (optionnal) 'default'| 'mangled' |'none' style
+ * @param mangled (optionnal) boolean
  * @param hash (optionnal) hash for html class
  */
 const TrilogyProviderStyled = ({
   children,
-  theme = 'default',
+  mangled = false,
   hash: HASH = versionJSON.VERSION,
 }: TrilogyProviderStyledProps): JSX.Element => {
-  const [styled, setStyled] = React.useState<boolean>(false)
-  const [hash, setHash] = React.useState<string|undefined>(HASH)
+  const [styled, setStyled] = React.useState<boolean>(mangled)
+  const [hash, setHash] = React.useState<string | undefined>(HASH)
 
-  const StyleComponent = React.useMemo(() => {
-    switch (true) {
-      case theme === 'mangled' && hash === versionJSON.VERSION:
-        setStyled(true)
-        return React.lazy(() => import('@/components/styleComponent/mangled/styleComponentMangled'))
-
-      case theme === 'mangled' && hash !== versionJSON.VERSION:
-        setStyled(true)
-        return undefined
-
-      case theme === 'default':
-        return React.lazy(() => import('@/components/styleComponent/default/styleComponent'))
-
-      default:
-        return undefined
-    }
-  }, [theme, hash])
-
-  return (
-    <TrilogyContext.Provider value={{ styled, setStyled, hash, setHash }}>
-      {StyleComponent ? (
-        <React.Suspense fallback={null}>
-          <StyleComponent>{children}</StyleComponent>
-        </React.Suspense>
-      ) : (
-        children
-      )}
-    </TrilogyContext.Provider>
-  )
+  return <TrilogyContext.Provider value={{ styled, setStyled, hash, setHash }}>{children}</TrilogyContext.Provider>
 }
 
 export { TrilogyProviderStyled }
